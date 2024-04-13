2d4fbd16cdee6048aaecdcf22949583627ad4284
Alexander Pevzner <pzz@apevzner.com>
2024-04-13 15:35:27 +0300

    IPP: more keywords types
    - Added KwPdlOverride
    - Added KwPrinterStateReasons

--- ipp/keywords.go
+++ ipp/keywords.go
@@ -8,10 +8,14 @@
 
 package ipp
 
-import "reflect"
+import (
+	"reflect"
+	"strings"
+)
 
 // KwCompression represents standard keyword values for compression.
-// See RFC8011, 5.4.32
+//
+// See RFC8011, 5.4.32.
 type KwCompression string
 
 const (
@@ -28,7 +32,121 @@ const (
 	KwCompressionCompress KwCompression = "compress"
 )
 
+// KwPdlOverride represents standard keyword values for
+// "pdl-override-supported" attribute.
+//
+// See RFC8011, 5.4.28.
+type KwPdlOverride string
+
+const (
+	// KwPdlOverrideAattempted indicates that Printer attempts to
+	// make the IPP attribute values take precedence over embedded
+	// instructions in the Document data.
+	KwPdlOverrideAattempted KwPdlOverride = "attempted"
+
+	// KwPdlOverrideNotAttempted indicates that the Printer makes no
+	// attempt to make the IPP attribute values take precedence over
+	// embedded instructions in the Document data.
+	KwPdlOverrideNotAttempted KwPdlOverride = "not-attempted"
+)
+
+// KwPrinterStateReasons represents standard keyword values for
+// "printer-state-reasons" attribute.
+//
+// See RFC8011, 5.4.12.
+type KwPrinterStateReasons string
+
+// Standard values for KwPrinterStateReasons
+const (
+	// The following are standard keyword values of "printer-state-reasons"
+	// attribute:
+	KwPrinterStateNone                           KwPrinterStateReasons = "none"
+	KwPrinterStateOther                          KwPrinterStateReasons = "other"
+	KwPrinterStateConnectingToDevice             KwPrinterStateReasons = "connecting-to-device"
+	KwPrinterStateCoverOpen                      KwPrinterStateReasons = "cover-open"
+	KwPrinterStateDeveloperEmpty                 KwPrinterStateReasons = "developer-empty"
+	KwPrinterStateDeveloperLow                   KwPrinterStateReasons = "developer-low"
+	KwPrinterStateDoorOpen                       KwPrinterStateReasons = "door-open"
+	KwPrinterStateFuserOverTemp                  KwPrinterStateReasons = "fuser-over-temp"
+	KwPrinterStateFuserUnderTemp                 KwPrinterStateReasons = "fuser-under-temp"
+	KwPrinterStateInputTrayMissing               KwPrinterStateReasons = "input-tray-missing"
+	KwPrinterStateInterlockOpen                  KwPrinterStateReasons = "interlock-open"
+	KwPrinterStateInterpreterResourceUnavailable KwPrinterStateReasons = "interpreter-resource-unavailable"
+	KwPrinterStateMarkerSupplyEmpty              KwPrinterStateReasons = "marker-supply-empty"
+	KwPrinterStateMarkerSupplyLow                KwPrinterStateReasons = "marker-supply-low"
+	KwPrinterStateMarkerWasteAlmostFull          KwPrinterStateReasons = "marker-waste-almost-full"
+	KwPrinterStateMarkerWasteFull                KwPrinterStateReasons = "marker-waste-full"
+	KwPrinterStateMediaEmpty                     KwPrinterStateReasons = "media-empty"
+	KwPrinterStateMediaJam                       KwPrinterStateReasons = "media-jam"
+	KwPrinterStateMediaLow                       KwPrinterStateReasons = "media-low"
+	KwPrinterStateMediaNeeded                    KwPrinterStateReasons = "media-needed"
+	KwPrinterStateMovingToPaused                 KwPrinterStateReasons = "moving-to-paused"
+	KwPrinterStateOpcLifeOver                    KwPrinterStateReasons = "opc-life-over"
+	KwPrinterStateOpcNearEol                     KwPrinterStateReasons = "opc-near-eol"
+	KwPrinterStateOutputAreaAlmostFull           KwPrinterStateReasons = "output-area-almost-full"
+	KwPrinterStateOutputAreaFull                 KwPrinterStateReasons = "output-area-full"
+	KwPrinterStateOutputTrayMissing              KwPrinterStateReasons = "output-tray-missing"
+	KwPrinterStatePaused                         KwPrinterStateReasons = "paused"
+	KwPrinterStateShutdown                       KwPrinterStateReasons = "shutdown"
+	KwPrinterStateSpoolAreaFull                  KwPrinterStateReasons = "spool-area-full"
+	KwPrinterStateStoppedPartly                  KwPrinterStateReasons = "stopped-partly"
+	KwPrinterStateStopping                       KwPrinterStateReasons = "stopping"
+	KwPrinterStateTimedOut                       KwPrinterStateReasons = "timed-out"
+	KwPrinterStateTonerEmpty                     KwPrinterStateReasons = "toner-empty"
+	KwPrinterStateTonerLow                       KwPrinterStateReasons = "toner-low"
+
+	// "printer-state-reasons" may also have one of the following
+	// standard suffixes, indicating its level of severity:
+	KwPrinterStateReport  KwPrinterStateReasons = "-report"
+	KwPrinterStateWarning KwPrinterStateReasons = "-warning"
+	KwPrinterStateError   KwPrinterStateReasons = "-error"
+)
+
+// Split splits KwPrinterStateReasons string into reason and severity suffix
+// (which can be one of "-report", "-warning" or "-error".
+//
+// If string contains one of the known suffixes, reason itself and
+// suffix will be returned separately. Otherwise, the first returned
+// value will be unmodified string, and second will be "":
+//
+//     "media-low-warning"  ->  "media-low", "-warning"
+//     "media-jam-error"    ->  "media-jam", "-error"
+//     "shutdown"           ->  "shutdown",  ""
+func (s KwPrinterStateReasons) Split() (
+	reason, severity KwPrinterStateReasons) {
+
+	idx := strings.LastIndexByte(string(s), '-')
+
+	if idx >= 0 {
+		prefix, suffix := s[:idx], s[idx:]
+		switch suffix {
+		case KwPrinterStateReport,
+			KwPrinterStateWarning,
+			KwPrinterStateError:
+			return prefix, suffix
+		}
+	}
+
+	return s, ""
+}
+
+// Reason splits KwPrinterStateReasons into reason and severity
+// and returns its reason part.
+func (s KwPrinterStateReasons) Reason() KwPrinterStateReasons {
+	reason, _ := s.Split()
+	return reason
+}
+
+// Severity splits KwPrinterStateReasons into reason and severity
+// and returns its severity part.
+func (s KwPrinterStateReasons) Severity() KwPrinterStateReasons {
+	_, severity := s.Split()
+	return severity
+}
+
 // kwRegisteredTypes lists all registered keyword types for IPP codec.
 var kwRegisteredTypes = map[reflect.Type]struct{}{
-	reflect.TypeOf(KwCompression("")): struct{}{},
+	reflect.TypeOf(KwCompression("")):         struct{}{},
+	reflect.TypeOf(KwPdlOverride("")):         struct{}{},
+	reflect.TypeOf(KwPrinterStateReasons("")): struct{}{},
 }
