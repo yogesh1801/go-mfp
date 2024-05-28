f30c0a2cab0705e236931058e926369974cd3d73
Alexander Pevzner <pzz@apevzner.com>
2024-05-28 14:05:20 +0300

    ipp: Encode test for CUPS structures updated

--- ipp/cups_test.go
+++ ipp/cups_test.go
@@ -9,7 +9,7 @@
 package ipp
 
 import (
-	"os"
+	"bytes"
 	"testing"
 
 	"github.com/OpenPrinting/goipp"
@@ -23,34 +23,43 @@ var (
 // TestCupsRequests tests CUPS requests
 func TestCupsRequests(t *testing.T) {
 	type testData struct {
-		rq     Request      // Request structure
-		groups goipp.Groups // Its IPP representation
+		rq  Request       // Request structure
+		msg goipp.Message // Its IPP representation
 	}
 
 	tests := []testData{
 		{
 			rq: &CUPSGetDefaultRequest{
+				Version:   goipp.DefaultVersion,
+				RequestID: 12345,
+
 				AttributesCharset:         DefaultCharset,
 				AttributesNaturalLanguage: DefaultNaturalLanguage,
 				RequestedAttributes:       []string{"all"},
 			},
 
-			groups: []goipp.Group{
-				{
-					Tag: goipp.TagOperationGroup,
-					Attrs: []goipp.Attribute{
-						goipp.MakeAttribute(
-							"attributes-charset",
-							goipp.TagCharset,
-							goipp.String(DefaultCharset)),
-						goipp.MakeAttribute(
-							"attributes-natural-language",
-							goipp.TagLanguage,
-							goipp.String(DefaultNaturalLanguage)),
-						goipp.MakeAttribute(
-							"requested-attributes",
-							goipp.TagKeyword,
-							goipp.String("all")),
+			msg: goipp.Message{
+				Version:   goipp.DefaultVersion,
+				Code:      goipp.Code(goipp.OpCupsGetDefault),
+				RequestID: 12345,
+
+				Groups: []goipp.Group{
+					{
+						Tag: goipp.TagOperationGroup,
+						Attrs: []goipp.Attribute{
+							goipp.MakeAttribute(
+								"attributes-charset",
+								goipp.TagCharset,
+								goipp.String(DefaultCharset)),
+							goipp.MakeAttribute(
+								"attributes-natural-language",
+								goipp.TagLanguage,
+								goipp.String(DefaultNaturalLanguage)),
+							goipp.MakeAttribute(
+								"requested-attributes",
+								goipp.TagKeyword,
+								goipp.String("all")),
+						},
 					},
 				},
 			},
@@ -59,28 +68,79 @@ func TestCupsRequests(t *testing.T) {
 
 	for _, test := range tests {
 		msg := test.rq.Encode()
-		msg.Print(os.Stdout, true)
+		if !msg.Similar(test.msg) {
+			buf := &bytes.Buffer{}
+
+			t.Errorf("Encode error. Message expected:")
+			test.msg.Print(buf, true)
+			t.Errorf("Message expected:\n%s", buf)
+
+			buf.Reset()
+			msg.Print(buf, true)
+			t.Errorf("Message received:\n%s", buf)
+		}
 	}
 }
 
+// TestCupsRequests tests CUPS responses
 func TestCupsRequesponses(t *testing.T) {
 	type testData struct {
 		rsp Response
+		msg goipp.Message // Its IPP representation
 	}
 
 	tests := []testData{
 		{
 			rsp: &CUPSGetDefaultResponse{
+				Version:   goipp.DefaultVersion,
+				Status:    goipp.StatusOk,
+				RequestID: 12345,
+
 				AttributesCharset:         DefaultCharset,
 				AttributesNaturalLanguage: DefaultNaturalLanguage,
 				StatusMessage:             "success",
+			},
+
+			msg: goipp.Message{
+				Version:   goipp.DefaultVersion,
+				Code:      goipp.Code(goipp.StatusOk),
+				RequestID: 12345,
+
+				Groups: []goipp.Group{
+					{
+						Tag: goipp.TagOperationGroup,
+						Attrs: []goipp.Attribute{
+							goipp.MakeAttribute(
+								"attributes-charset",
+								goipp.TagCharset,
+								goipp.String(DefaultCharset)),
+							goipp.MakeAttribute(
+								"attributes-natural-language",
+								goipp.TagLanguage,
+								goipp.String(DefaultNaturalLanguage)),
+							goipp.MakeAttribute(
+								"status-message",
+								goipp.TagText,
+								goipp.String("success")),
+						},
+					},
+				},
 			},
 		},
 	}
 
 	for _, test := range tests {
 		msg := test.rsp.Encode()
+		if !msg.Similar(test.msg) {
+			buf := &bytes.Buffer{}
 
-		msg.Print(os.Stdout, false)
+			t.Errorf("Encode error. Message expected:")
+			test.msg.Print(buf, false)
+			t.Errorf("Message expected:\n%s", buf)
+
+			buf.Reset()
+			msg.Print(buf, false)
+			t.Errorf("Message received:\n%s", buf)
+		}
 	}
 }
