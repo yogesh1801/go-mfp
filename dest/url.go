8eb655694a2c0d3c425ca93157e722b6e46db34c
Alexander Pevzner <pzz@apevzner.com>
2024-06-03 00:33:53 +0300

    dest: added ParseUserURL() prototype

--- dest/url.go
+++ dest/url.go
@@ -19,6 +19,41 @@ var (
 	errInvalidURLScheme = errors.New("Printer URL: scheme must be ipp or ipps")
 )
 
+// ParseUserURL parses URL, entered by user. The 'addr'
+// parameter must be either URL with schema set to "ipp",
+// "ipps", "http" or "https" or IPv4 or IPv6 literal address
+// optionally followed by port. If IPv6 address is used,
+// it must be enclosed into the square brackets. IPv6
+// may specify a zone suffix, as defined in [RFC 4007].
+//
+// The 'path' parameter is only used if URL is specified as
+// IP literal. At this case, it defines path part of the
+// URL. In other cases, the 'path' parameter is ignored.
+//
+// The path part of parsed URL is normalized according
+// rules defined by the [RFC 3986].
+//
+// Examples:
+//
+//	ipp://host/...         IPP URL, port 631
+//	ipp://host:port/...    IPP URL, specified port
+//	ipps://host/...        IPPS URL, port 631
+//	ipps://host:port/...   IPPS URL, specified port
+//	http://host/...        IPP URL, port 80
+//	http://host:port/...   IPP URL, specified port
+//	https://host/...       IPPS URL, port 443
+//	https://host:port/...  IPPS URL, specified port
+//	IP4-ADDR               ipp://IP4-ADDR:631/path
+//	IP4-ADDR:PORT          ipp://IP4-ADDR:PORT/path
+//	[IP4-ADDR]             ipp://IP6-ADDR:631/path
+//	[IP6-ADDR]:PORT        ipp://IP6-ADDR:PORT/path
+//
+// [RFC 3986]: https://www.rfc-editor.org/rfc/rfc3986.html
+// [RFC 4007]: https://www.rfc-editor.org/rfc/rfc4007.html
+func ParseUserURL(addr, path string) (*url.URL, error) {
+	return nil, nil
+}
+
 // urlParse parses IPP URL ("ipp://..." or "ipps://...")
 //
 // It returns parsed HTTP URL (with "http" or "https" scheme"),
