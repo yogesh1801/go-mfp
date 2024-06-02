a47c63c86adbe6066c7c93167bb7656dee92a101
Alexander Pevzner <pzz@apevzner.com>
2024-06-02 22:37:53 +0300

    ipp: url_test.go refactored

--- ipp/url_test.go
+++ ipp/url_test.go
@@ -13,87 +13,87 @@ import (
 	"testing"
 )
 
-type urlParseTest struct {
-	in, ipp, http string // Input and expected IPP/HTTP output
-	err           error  // Expected error
-}
-
-var urlParseTestData = []urlParseTest{
-	{
-		in:   "ipp://127.0.0.1/ipp/print",
-		ipp:  "ipp://127.0.0.1/ipp/print",
-		http: "http://127.0.0.1:631/ipp/print",
-	},
-	{
-		in:   "ipp://127.0.0.1/ipp/xxx/../print",
-		ipp:  "ipp://127.0.0.1/ipp/print",
-		http: "http://127.0.0.1:631/ipp/print",
-	},
-	{
-		in:   "ipp://127.0.0.1/../ipp/print",
-		ipp:  "ipp://127.0.0.1/ipp/print",
-		http: "http://127.0.0.1:631/ipp/print",
-	},
-	{
-		in:   "ipp://127.0.0.1:80/ipp/print",
-		ipp:  "ipp://127.0.0.1:80/ipp/print",
-		http: "http://127.0.0.1/ipp/print",
-	},
-	{
-		in:   "ipps://127.0.0.1:443/ipp/print",
-		ipp:  "ipps://127.0.0.1:443/ipp/print",
-		http: "https://127.0.0.1/ipp/print",
-	},
-	{
-		in:   "ipp://127.0.0.1:631/ipp/print",
-		ipp:  "ipp://127.0.0.1/ipp/print",
-		http: "http://127.0.0.1:631/ipp/print",
-	},
-	{
-		in:   "ipp://[fe80::aec5:1bff:fe1c:6fa7%252]/ipp/print",
-		ipp:  "ipp://[fe80::aec5:1bff:fe1c:6fa7%252]/ipp/print",
-		http: "http://[fe80::aec5:1bff:fe1c:6fa7%252]:631/ipp/print",
-	},
-	{
-		in:  "http://127.0.0.1/ipp/print",
-		err: errors.New("Printer URL: scheme must be ipp or ipps"),
-	},
-	{
-		in:  "https://127.0.0.1/ipp/print",
-		err: errors.New("Printer URL: scheme must be ipp or ipps"),
-	},
-	{
-		in:  "",
-		err: errors.New("Printer URL: invalid URL"),
-	},
-	{
-		in:  "http://Invalid URL",
-		err: errors.New("Printer URL: invalid URL"),
-	},
-}
-
 // TestUrlParse tests urlParse function
 func TestUrlParse(t *testing.T) {
-	for _, test := range urlParseTestData {
-		http, norm, err := urlParse(test.in)
+	type testData struct {
+		in, ipp, http string // Input and expected IPP/HTTP output
+		err           string // Expected error
+	}
 
-		switch {
-		case test.err == nil && err != nil:
-			t.Errorf("%s: unexpected error: %q", test.in, err)
-		case test.err != nil && err == nil:
-			t.Errorf("%s: no error, expected: %q", test.in,
-				test.err)
-		case test.err != nil && err != nil:
-			if test.err.Error() != err.Error() {
-				t.Errorf("%s: error expected %q, got %q",
+	tests := []testData{
+		{
+			in:   "ipp://127.0.0.1/ipp/print",
+			ipp:  "ipp://127.0.0.1/ipp/print",
+			http: "http://127.0.0.1:631/ipp/print",
+		},
+		{
+			in:   "ipp://127.0.0.1/ipp/xxx/../print",
+			ipp:  "ipp://127.0.0.1/ipp/print",
+			http: "http://127.0.0.1:631/ipp/print",
+		},
+		{
+			in:   "ipp://127.0.0.1/../ipp/print",
+			ipp:  "ipp://127.0.0.1/ipp/print",
+			http: "http://127.0.0.1:631/ipp/print",
+		},
+		{
+			in:   "ipp://127.0.0.1:80/ipp/print",
+			ipp:  "ipp://127.0.0.1:80/ipp/print",
+			http: "http://127.0.0.1/ipp/print",
+		},
+		{
+			in:   "ipps://127.0.0.1:443/ipp/print",
+			ipp:  "ipps://127.0.0.1:443/ipp/print",
+			http: "https://127.0.0.1/ipp/print",
+		},
+		{
+			in:   "ipp://127.0.0.1:631/ipp/print",
+			ipp:  "ipp://127.0.0.1/ipp/print",
+			http: "http://127.0.0.1:631/ipp/print",
+		},
+		{
+			in:   "ipp://[fe80::aec5:1bff:fe1c:6fa7%252]/ipp/print",
+			ipp:  "ipp://[fe80::aec5:1bff:fe1c:6fa7%252]/ipp/print",
+			http: "http://[fe80::aec5:1bff:fe1c:6fa7%252]:631/ipp/print",
+		},
+		{
+			in:  "http://127.0.0.1/ipp/print",
+			err: "Printer URL: scheme must be ipp or ipps",
+		},
+		{
+			in:  "https://127.0.0.1/ipp/print",
+			err: "Printer URL: scheme must be ipp or ipps",
+		},
+		{
+			in:  "",
+			err: "Printer URL: invalid URL",
+		},
+		{
+			in:  "http://Invalid URL",
+			err: "Printer URL: invalid URL",
+		},
+	}
+
+	for _, test := range tests {
+		http, norm, err := urlParse(test.in)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if test.err != "" {
+			if err.Error() != test.err {
+				t.Errorf("%s: error expected: %q, present: %q",
 					test.in, test.err, err)
 			}
-		case norm != test.ipp:
-			t.Errorf("%s: IPP expected %q, got %q", test.in,
-				test.ipp, norm)
-		case http.String() != test.http:
-			t.Errorf("%s: HTTP expected %q, got %q", test.in,
-				test.http, http.String())
+		} else {
+			if norm != test.ipp {
+				t.Errorf("%s: IPP URL expected %q, present %q", test.in,
+					test.ipp, norm)
+			}
+			if http.String() != test.http {
+				t.Errorf("%s: HTTP URL expected %q, got %q", test.in,
+					test.http, http.String())
+			}
 		}
 	}
 }
