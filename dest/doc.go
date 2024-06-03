1eab88b24ab45129d49b03866655be8ca8958b7c
Alexander Pevzner <pzz@apevzner.com>
2024-06-03 17:13:35 +0300

    dest: added placeholder for package documentation

---
+++ dest/doc.go
@@ -0,0 +1,9 @@
+// MFP  - Miulti-Function Printers and scanners toolkit
+// DEST - Destination URLs hanling
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Package documentation
+
+package dest
