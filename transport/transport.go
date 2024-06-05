d6f84d6ec0aee9bd8dca934602a5e0ba16836b01
Alexander Pevzner <pzz@apevzner.com>
2024-06-05 13:35:59 +0300

    transport: added Transport for HTTP clients
    Transport wraps http.Transport and implements http.RoundTripper interface
    with added support for the "ipp", "ipps" and "unix" schemes.

---
+++ transport/transport.go
@@ -0,0 +1,215 @@
+// MFP       - Miulti-Function Printers and scanners toolkit
+// TRANSPORT - Transport protocol implementation
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// HTTP Transport implementation
+
+package transport
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+var defaultDiaaler net.Dialer
+
+// Transport wraps [http.Transport] and adds the following functionality:
+//
+//   - "ipp", "ipps" schemes support.
+//   - "unix" schema support for connecting via AF_UNIX sockets.
+type Transport struct {
+	*http.Transport
+	templateDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
+}
+
+// NewTransport creates a new Transport. Provided [http.Transport]r
+// is only used as a configuration template.
+func NewTransport(template *http.Transport) *Transport {
+	if template == nil {
+		template = http.DefaultTransport.(*http.Transport)
+	}
+
+	tr := &Transport{
+		Transport:           template.Clone(),
+		templateDialContext: template.DialContext,
+	}
+
+	tr.DialContext = tr.dialContect
+
+	return tr
+}
+
+// RoundTrip executes a single HTTP transaction, returning
+// a Response for the provided Request.
+func (tr *Transport) RoundTrip(rq *http.Request) (*http.Response, error) {
+	oldURL := rq.URL
+	if oldURL == nil {
+		return tr.Transport.RoundTrip(rq)
+	}
+
+	newURL := &url.URL{}
+	*newURL = *oldURL
+
+	// Here we hack the Request URL:
+	//   - scheme always set to "http" or "https"
+	//   - underlying socket-level protocol ("tcp" or "unix")
+	//     embedded into the Host
+	//   - for "unix", path also embedded into the Host
+	//
+	// Then dialContect() can decode this information from the
+	// supplied address and use appropriately.
+	host := newURL.Hostname()
+	port := newURL.Port()
+	defaultPort := ""
+	proto := "tcp"
+
+	switch newURL.Scheme {
+	case "ipp":
+		newURL.Scheme = "http"
+		defaultPort = "631"
+
+	case "ipps":
+		newURL.Scheme = "https"
+		defaultPort = "631"
+
+	case "http":
+		port = "80"
+		proto = "tcp"
+
+	case "https":
+		port = "443"
+		proto = "tcp"
+
+	case "unix":
+		newURL.Scheme = "http"
+		host = escapePath(newURL.Path)
+		port = "80"
+		proto = "unix"
+
+	default:
+		return tr.Transport.RoundTrip(rq)
+	}
+
+	if port == "" {
+		port = defaultPort
+	}
+
+	newURL.Host = net.JoinHostPort(proto+"+"+host, port)
+
+	// Replace Request URL with the hacked URL. Restore after use
+	defer func() { rq.URL = oldURL }()
+	rq.URL = newURL
+
+	return tr.Transport.RoundTrip(rq)
+}
+
+// dialContect implements DialContext callback for underlying
+// http.Transport.
+func (tr *Transport) dialContect(ctx context.Context,
+	network, addr string) (net.Conn, error) {
+
+	host, port, _ := net.SplitHostPort(addr)
+	network, host, _ = strings.Cut(host, "+")
+
+	addr = net.JoinHostPort(host, port)
+
+	if network == "unix" {
+		addr, _ = strings.CutSuffix(addr, ":"+port)
+		addr = unescapePath(addr)
+	}
+
+	dial := tr.templateDialContext
+	if dial == nil {
+		dial = defaultDiaaler.DialContext
+	}
+
+	return dial(ctx, network, addr)
+}
+
+// escapePath encodes path so it becomes syntactically correct
+// when passed as address to dialContext.
+//
+// Indented for unix:/ scheme implementation.
+func escapePath(in string) string {
+	const hex = "0123456789abcdef"
+
+	out := make([]byte, 0, len(in)*2)
+	for i := 0; i < len(in); i++ {
+		c := in[i]
+
+		if 'a' <= c && c <= 'z' ||
+			'A' <= c && c <= 'Z' ||
+			'0' <= c && c <= '9' {
+			out = append(out, c)
+		} else {
+			out = append(out, '-', hex[c>>4], hex[c&0xf])
+		}
+	}
+
+	return string(out)
+}
+
+// unescapePath decodes path, encoded by escapePath().
+func unescapePath(in string) string {
+	type state int
+	const (
+		stateNorm state = iota
+		stateEsc1
+		stateEsc2
+	)
+
+	out := make([]byte, 0, len(in))
+	decodeState := stateNorm
+	escval := 0
+
+	for i := 0; i < len(in); i++ {
+		c := in[i]
+
+		switch decodeState {
+		case stateNorm:
+			if c != '-' {
+				out = append(out, c)
+			} else {
+				decodeState = stateEsc1
+			}
+
+		case stateEsc1, stateEsc2:
+			v := hexval(c)
+			switch {
+			case v < 0:
+				out = append(out, c)
+				decodeState = stateNorm
+
+			case decodeState == stateEsc1:
+				escval = v
+				decodeState = stateEsc2
+
+			case decodeState == stateEsc2:
+				escval = (escval << 4) + v
+				out = append(out, byte(escval))
+				decodeState = stateNorm
+			}
+		}
+	}
+
+	return string(out)
+}
+
+// hexval returns hexadecimal value of c, or -1 if c is not
+// hexadecimal digit.
+func hexval(c byte) int {
+	switch {
+	case '0' <= c && c <= '9':
+		return int(c - '0')
+	case 'a' <= c && c <= 'f':
+		return int(c-'a') + 10
+	case 'A' <= c && c <= 'F':
+		return int(c-'A') + 10
+	}
+	return -1
+}
---
+++ transport/transport_test.go
@@ -0,0 +1,170 @@
+// MFP       - Miulti-Function Printers and scanners toolkit
+// TRANSPORT - Transport protocol implementation
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Transport test
+
+package transport
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"testing"
+)
+
+// TestEscapePath tests that unescapePath(escapePath(s)) == s
+func TestEscapePath(t *testing.T) {
+	tests := []string{
+		"",
+		"/usr/bin",
+		"usr:bin",
+	}
+
+	for _, test := range tests {
+		escaped := escapePath(test)
+		unescaped := unescapePath(escaped)
+
+		if unescaped != test {
+			t.Errorf("%q: escaped: %q, unescaped: %q",
+				test, escaped, unescaped)
+		}
+	}
+}
+
+// TestUnescapePath contains additional tests for code paths
+// not covered by the TestEscapePath() (mostly handling of
+// invalid sequences).
+func TestUnescapePath(t *testing.T) {
+	type testData struct {
+		escaped   string // Escaped string
+		unescaped string // Expected unescaped string
+	}
+
+	tests := []testData{
+		{
+			escaped:   "",
+			unescaped: "",
+		},
+
+		{
+			escaped:   "0-1",
+			unescaped: "0",
+		},
+
+		{
+			escaped:   "0-12",
+			unescaped: "0\x12",
+		},
+
+		{
+			escaped:   "0-1a",
+			unescaped: "0\x1a",
+		},
+
+		{
+			escaped:   "0-1A",
+			unescaped: "0\x1A",
+		},
+
+		{
+			escaped:   "0-1x",
+			unescaped: "0x",
+		},
+	}
+
+	for _, test := range tests {
+		unescaped := unescapePath(test.escaped)
+		if unescaped != test.unescaped {
+			t.Errorf("%q: expected: %q, present: %q",
+				test.escaped, test.unescaped, unescaped)
+		}
+	}
+}
+
+// TestTransportDial tests how Transport handles connection
+// to server, depending on the destination URL.
+func TestTransportDial(t *testing.T) {
+	type testData struct {
+		dest          string // Destination URL
+		network, addr string // dial network and address
+	}
+
+	tests := []testData{
+		{
+			dest:    "unix:/var/run/cups/cups.sock",
+			network: "unix",
+			addr:    "/var/run/cups/cups.sock",
+		},
+
+		{
+			dest:    "http://[::1]/",
+			network: "tcp",
+			addr:    "[::1]:80",
+		},
+
+		{
+			dest:    "http://[::1%25eth0]/",
+			network: "tcp",
+			addr:    "[::1%eth0]:80",
+		},
+	}
+
+	var network, addr string
+
+	dial := func(ctx context.Context, n, a string) (net.Conn, error) {
+		network, addr = n, a
+		return nil, errors.New("not implemented")
+	}
+
+	template := *(http.DefaultTransport.(*http.Transport))
+	template.DialContext = dial
+	tr := NewTransport(&template)
+
+	for _, test := range tests {
+		rq, err := http.NewRequest("GET", test.dest, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		tr.RoundTrip(rq)
+
+		if network != test.network {
+			t.Errorf("%q: network expected %q, present %q",
+				test.dest, test.network, network)
+		}
+
+		if addr != test.addr {
+			t.Errorf("%q: addr expected %q, present %q",
+				test.dest, test.addr, addr)
+		}
+	}
+}
+
+func TestTransport(t *testing.T) {
+
+	//return
+
+	rq, err := http.NewRequest("GET", "unix:/var/run/cups/cups.sock", nil)
+	//rq, err := http.NewRequest("GET", "http://localhost/", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	tr := NewTransport(nil)
+	rsp, err := tr.RoundTrip(rq)
+
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+
+	fmt.Printf("================\n")
+	rsp.Header.WriteSubset(os.Stdout, nil)
+}
