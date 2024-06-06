3c6efe7b0212bb0da9ff1a05dfce8f01fc2dda0d
Alexander Pevzner <pzz@apevzner.com>
2024-06-06 13:07:45 +0300

    transport: 100% test coverage in loopback.go

--- transport/loopback_test.go
+++ transport/loopback_test.go
@@ -10,6 +10,8 @@ package transport
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"io"
 	"net/http"
 	"sync"
@@ -75,5 +77,90 @@ func TestLoopbackBasic(t *testing.T) {
 			"expected: %q\n"+
 			"present:  %q",
 			message, body)
+	}
+}
+
+// TestLoopbackDoubleClose closes twice net.Listener, returned by
+// NewLoopback, and checks for results.
+func TestLoopbackDoubleClose(t *testing.T) {
+	_, l := NewLoopback()
+
+	err := l.Close()
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+		return
+	}
+
+	err = l.Close()
+	if err != ErrLoopbackClosed {
+		t.Errorf("Error expected %s, present %s",
+			ErrLoopbackClosed, err)
+	}
+}
+
+// TestLoopbackCloseUnblocksAll tests that closing Loobpack
+// net.Listener unblocks all waiters.
+func TestLoopbackCloseUnblocksAll(t *testing.T) {
+	tr, l := NewLoopback()
+
+	var done sync.WaitGroup
+
+	addr := l.Addr()
+
+	for i := 0; i < 10; i++ {
+		done.Add(1)
+		go func() {
+			tr.DialContext(context.Background(),
+				addr.Network(), addr.String())
+			done.Done()
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		done.Add(1)
+		go func() {
+			l.Accept()
+			done.Done()
+		}()
+	}
+
+	l.Close()
+	done.Wait()
+}
+
+// TestLoopbackBysyError tests that Loopback properly generates
+// ErrLoopbackBusy error when there are too many connections attempts.
+func TestLoopbackBysyError(t *testing.T) {
+	tr, l := NewLoopback()
+
+	addr := l.Addr()
+	connCount := 0
+
+	var err error
+
+	for i := 0; i < LoopbackMaxPendingConnections*2; i++ {
+		_, err = tr.DialContext(context.Background(),
+			addr.Network(), addr.String())
+
+		if err != nil {
+			break
+		}
+
+		connCount++
+	}
+
+	if err == nil {
+		err = errors.New("")
+	}
+
+	if err != ErrLoopbackBusy {
+		t.Errorf("Error expected: %q, present: %q",
+			ErrLoopbackBusy, err)
+		return
+	}
+
+	if connCount != LoopbackMaxPendingConnections {
+		t.Errorf("Connection count expected: %d, present: %d",
+			LoopbackMaxPendingConnections, connCount)
 	}
 }
