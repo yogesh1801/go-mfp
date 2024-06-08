b45518f2c0d52c8cd06811428d0b907541e153b2
Alexander Pevzner <pzz@apevzner.com>
2024-06-09 02:56:15 +0300

    transport: added DefaultCupsSocket constant

---
+++ transport/default.go
@@ -0,0 +1,14 @@
+// MFP       - Miulti-Function Printers and scanners toolkit
+// TRANSPORT - Transport protocol implementation
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Default values
+
+package transport
+
+const (
+	// DefaultCupsSocket is the default CUPS socket URL
+	DefaultCupsSocket = "unix:/var/run/cups/cups.sock"
+)
