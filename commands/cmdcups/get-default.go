67a24d4240033c3e6d909f414be544d7742fea41
Alexander Pevzner <pzz@apevzner.com>
2024-06-24 22:51:11 +0300

    cmdcups: attrs parameter replaced with --attrs option, added completion

--- commands/cmdcups/get-default.go
+++ commands/cmdcups/get-default.go
@@ -11,11 +11,13 @@ package cmdcups
 import (
 	"context"
 	"sort"
+	"strings"
 
 	"github.com/OpenPrinting/goipp"
 	"github.com/alexpevzner/mfp/argv"
 	"github.com/alexpevzner/mfp/cups"
 	"github.com/alexpevzner/mfp/env"
+	"github.com/alexpevzner/mfp/ipp"
 	"github.com/alexpevzner/mfp/transport"
 )
 
@@ -25,13 +27,14 @@ var cmdGetDefault = argv.Command{
 	Help:    "Get default printer",
 	Handler: cmdGetDefaultHandler,
 	Options: []argv.Option{
-		argv.HelpOption,
-	},
-	Parameters: []argv.Parameter{
 		{
-			Name: "[attrs...]",
-			Help: "Requested attributes",
+			Name:     "-a",
+			Aliases:  []string{"--attrs"},
+			Help:     "Additional attributes",
+			Validate: argv.ValidateAny,
+			Complete: completeAttrName,
 		},
+		argv.HelpOption,
 	},
 }
 
@@ -43,7 +46,7 @@ func cmdGetDefaultHandler(ctx context.Context, inv *argv.Invocation) error {
 		dest = transport.MustParseAddr(addr, "ipp://localhost/")
 	}
 
-	attrList := inv.Values("attrs")
+	attrList := getAttrsParameter(inv)
 	attrList = append(attrList, "printer-name", "printer-uri-supported")
 
 	pager := env.NewPager()
@@ -73,4 +76,40 @@ func cmdGetDefaultHandler(ctx context.Context, inv *argv.Invocation) error {
 	f.WriteTo(pager)
 
 	return pager.Display()
+}
+
+// getAttrsParameter extracts and sanitizes list of
+// requested attributes
+func getAttrsParameter(inv *argv.Invocation) (attrs []string) {
+	params := inv.Values("--attrs")
+	for _, param := range params {
+		for _, name := range strings.Split(param, ",") {
+			if name != "" {
+				attrs = append(attrs, name)
+			}
+		}
+	}
+
+	return
+}
+
+// completeAttrName is the completion callback for requested attributes
+func completeAttrName(arg string) (compl []string, flags argv.CompleterFlags) {
+	infos := ((*ipp.PrinterAttributes)(nil)).KnownAttrs()
+
+	attrName := arg
+	prefix := ""
+
+	if i := strings.LastIndex(attrName, ","); i >= 0 {
+		attrName = arg[i+1:]
+		prefix = arg[:i]
+	}
+
+	for _, info := range infos {
+		if strings.HasPrefix(info.Name, attrName) {
+			compl = append(compl, prefix+info.Name)
+		}
+	}
+
+	return
 }
