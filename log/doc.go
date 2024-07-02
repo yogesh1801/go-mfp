ff1c5acfc834b52867b72c6b8443912404e06937
Alexander Pevzner <pzz@apevzner.com>
2024-07-02 17:45:51 +0300

    log: cosmetic

--- log/doc.go
+++ log/doc.go
@@ -1,5 +1,5 @@
 // MFP - Miulti-Function Printers and scanners toolkit
-// DNS-SD service discovery
+// Logging facilities
 //
 // Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
 // See LICENSE for license terms and conditions
