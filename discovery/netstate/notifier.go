92a0e868690021adc54a2827d3817cefca582eaa
Alexander Pevzner <pzz@apevzner.com>
2024-07-18 20:48:04 +0300

    discovery/netstate: Notifier made safe for use with multiple goroutines

--- discovery/netstate/notifier.go
+++ discovery/netstate/notifier.go
@@ -12,23 +12,30 @@ import "context"
 
 // Notifier provides network state change events notifications.
 type Notifier struct {
-	snapLast snapshot   // Last network state known to the client
-	queue    eventqueue // Queue of not yet delivered events
-	errSeq   int64      // Sequence number of next error
+	snapLast snapshot      // Last network state known to the client
+	queue    eventqueue    // Queue of not yet delivered events
+	errSeq   int64         // Sequence number of next error
+	lockChan chan struct{} // Channel-based lock
 }
 
 // NewNotifier creates a new Notifier.
+//
+// This is safe to use Notifier with multiple goroutines.
 func NewNotifier() *Notifier {
-	return &Notifier{}
+	return &Notifier{
+		lockChan: make(chan struct{}, 1),
+	}
 }
 
 // Get waits until and returns a next [Event].
 // The only case when it returns an error is the context cancellation.
 func (not *Notifier) Get(ctx context.Context) (Event, error) {
-	// Quick check for a pending Context error
-	if err := ctx.Err(); err != nil {
+	// Acquire the lock
+	if err := not.lock(ctx); err != nil {
 		return nil, err
 	}
+
+	defer not.unlock()
 
 	// Check for queued events
 	if evnt := not.queue.pull(); evnt != nil {
@@ -59,4 +66,22 @@ func (not *Notifier) Get(ctx context.Context) (Event, error) {
 			return nil, ctx.Err()
 		}
 	}
+}
+
+// lock acquires the lock.
+//
+// If lock is busy, it waits until lock is available or Context
+// is canceled.
+func (not *Notifier) lock(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case not.lockChan <- struct{}{}:
+		return nil
+	}
+}
+
+// unlock releases the lock, previously acquired by Notifier.lock.
+func (not *Notifier) unlock() {
+	<-not.lockChan
 }
