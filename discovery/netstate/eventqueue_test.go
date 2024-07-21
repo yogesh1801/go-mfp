37b802617e580c5c38bde6476bb1ab889c4b1ba4
Alexander Pevzner <pzz@apevzner.com>
2024-07-21 15:08:38 +0300

    discovery/netstate: added event queue test

---
+++ discovery/netstate/eventqueue_test.go
@@ -0,0 +1,44 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// Network state monitoring
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Event queue test
+
+package netstate
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestEventQueue(t *testing.T) {
+	netifmaker := testNewNetIfMaker()
+	if0 := netifmaker.new()
+	a4 := *testMakeAddr(if0, "127.0.0.1/24")
+	a6 := *testMakeAddr(if0, "::1/24")
+
+	events := []Event{
+		EventAddInterface{if0},
+		EventAddAddress{a4},
+		EventAddAddress{a6},
+		EventError{errors.New("very serious error")},
+		EventDelAddress{a6},
+		EventDelAddress{a4},
+		EventDelInterface{if0},
+	}
+
+	var q eventqueue
+	for _, evnt := range events {
+		q.push(evnt)
+	}
+
+	for _, expected := range append(events, nil) {
+		evnt := q.pull()
+		if !reflect.DeepEqual(evnt, expected) {
+			t.Errorf("expected: %v, present: %v", expected, evnt)
+		}
+	}
+}
