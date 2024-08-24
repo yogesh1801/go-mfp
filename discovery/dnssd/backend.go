982d6388707d4df99003eea11614868a4234c4c1
Alexander Pevzner <pzz@apevzner.com>
2024-08-24 13:01:31 +0300

    discovery/dnssd: refactoring

--- discovery/dnssd/backend.go
+++ discovery/dnssd/backend.go
@@ -11,7 +11,6 @@ package dnssd
 import (
 	"context"
 	"fmt"
-	"net"
 	"net/netip"
 	"sync"
 	"time"
@@ -19,16 +18,6 @@ import (
 	"github.com/alexpevzner/go-avahi"
 	"github.com/alexpevzner/mfp/discovery"
 	"github.com/alexpevzner/mfp/log"
-)
-
-// Parameters:
-const (
-	// Avahi client may fail to start if avahi-daemon is not
-	// running for for the similar reasons.
-	//
-	// If failed, Avahi client will automatically restart
-	// with the following interval between attempts.
-	avahiClientRestartInterval = 1 * time.Second
 )
 
 // backend is the [discovery.Backend] for DNS-SD discovery
@@ -201,7 +190,7 @@ func (back *backend) startServiceBrowsers() error {
 			avahi.ProtocolUnspec,
 			svctype,
 			back.domain,
-			avahiLookupFlags[back.lookupFlags],
+			avahiLookupFlags(back.lookupFlags),
 		)
 
 		title := fmt.Sprintf("svc-browse: start %q", svctype)
@@ -405,7 +394,7 @@ func (back *backend) getService(key avahiServiceKey) *avahiService {
 // in the back.poller and back.services.
 func (back *backend) addService(key avahiServiceKey) error {
 	// Create service resolver
-	flags := avahiLookupFlags[back.lookupFlags]
+	flags := avahiLookupFlags(back.lookupFlags)
 	flags |= avahi.LookupNoAddress | avahi.LookupNoTXT
 
 	svcResolver, err := avahi.NewServiceResolver(
@@ -436,7 +425,7 @@ func (back *backend) addService(key avahiServiceKey) error {
 		key.FQDN(),
 		avahi.DNSClassIN,
 		avahi.DNSTypeTXT,
-		avahiLookupFlags[back.lookupFlags],
+		avahiLookupFlags(back.lookupFlags),
 	)
 
 	title = fmt.Sprintf("txt-browse: start %s", key)
@@ -544,7 +533,7 @@ func (back *backend) addHostname(key avahiHostnameKey) (*avahiHostname, error) {
 		key.Hostname,
 		avahi.DNSClassIN,
 		rtype,
-		avahiLookupFlags[back.lookupFlags],
+		avahiLookupFlags(back.lookupFlags),
 	)
 
 	title := fmt.Sprintf("addr-browse: start %s", key)
@@ -576,156 +565,4 @@ func (back *backend) delHostname(hostname *avahiHostname) {
 	hostname.addrBrowser.Close()
 
 	delete(back.hostnames, hostname.key)
-}
-
-// avahiLookupFlags maps LookupFlags to avahi.LookupFlags
-var avahiLookupFlags = [...]avahi.LookupFlags{
-	0:               avahi.LookupUseMulticast,
-	LookupClassical: avahi.LookupUseWideArea,
-	LookupMulticast: avahi.LookupUseMulticast,
-	LookupBoths:     avahi.LookupUseMulticast,
-}
-
-// avahiService is the per-service-instance structure
-// that manages resources associated with the service
-type avahiService struct {
-	key         avahiServiceKey        // Identity
-	svcResolver *avahi.ServiceResolver // Service resolver
-	txtBrowser  *avahi.RecordBrowser   // TXT record resolver
-	port        uint16                 // IP port
-	hostname    *avahiHostname         // Hostname resolver
-}
-
-// avahiServiceKey identifies a particular instance of
-// service of the particular type.
-type avahiServiceKey struct {
-	IfIdx        avahi.IfIndex  // Network interface index
-	Proto        avahi.Protocol // Network protocol
-	InstanceName string         // Service instance name
-	SvcType      string         // Service type
-	Domain       string         // Service domain
-}
-
-// FQDN returns the full-qualified domain name for the
-// service instance.
-func (key avahiServiceKey) FQDN() string {
-	return avahi.DomainServiceNameJoin(key.InstanceName,
-		key.SvcType, key.Domain)
-}
-
-// String returns string representation of the avahiServiceKey,
-// for debugging.
-func (key avahiServiceKey) String() string {
-	var ifname string
-
-	if ifi, err := net.InterfaceByIndex(int(key.IfIdx)); err == nil {
-		ifname = ifi.Name
-	} else {
-		ifname = fmt.Sprintf("%d", key.IfIdx)
-	}
-
-	return fmt.Sprintf("%q (%s,%s)", key.FQDN(), key.Proto, ifname)
-}
-
-// avahiServiceKeyFromServiceBrowserEvent makes avahiServiceKey
-// from the avahi.ServiceBrowserEvent.
-func avahiServiceKeyFromServiceBrowserEvent(
-	evnt *avahi.ServiceBrowserEvent) avahiServiceKey {
-
-	return avahiServiceKey{
-		IfIdx:        evnt.IfIdx,
-		Proto:        evnt.Proto,
-		InstanceName: evnt.InstanceName,
-		SvcType:      evnt.SvcType,
-		Domain:       evnt.Domain,
-	}
-}
-
-// avahiServiceKeyFromResolverEvent makes avahiServiceKey
-// from the avahi.ServiceResolverEvent.
-func avahiServiceKeyFromResolverEvent(
-	evnt *avahi.ServiceResolverEvent) avahiServiceKey {
-
-	return avahiServiceKey{
-		IfIdx:        evnt.IfIdx,
-		Proto:        evnt.Proto,
-		InstanceName: evnt.InstanceName,
-		SvcType:      evnt.SvcType,
-		Domain:       evnt.Domain,
-	}
-}
-
-// avahiServiceKeyFromRecordBrowserEvent makes avahiServiceKey
-// from the avahi.RecordBrowserEvent
-func avahiServiceKeyFromRecordBrowserEvent(
-	evnt *avahi.RecordBrowserEvent) avahiServiceKey {
-
-	instance, svctype, domain := avahi.DomainServiceNameSplit(evnt.Name)
-
-	return avahiServiceKey{
-		IfIdx:        evnt.IfIdx,
-		Proto:        evnt.Proto,
-		InstanceName: instance,
-		SvcType:      svctype,
-		Domain:       domain,
-	}
-}
-
-// avahiHostname is the per-hostname structure that manages
-// resources associated with the hostname
-type avahiHostname struct {
-	key         avahiHostnameKey           // Identity
-	addrBrowser *avahi.RecordBrowser       // A/AAAA record resolver
-	addrs       map[netip.Addr]struct{}    // Resolved addresses
-	services    map[*avahiService]struct{} // Dependent services
-}
-
-// hasAddr reports if address already known
-func (hostname *avahiHostname) hasAddr(addr netip.Addr) bool {
-	_, found := hostname.addrs[addr]
-	return found
-}
-
-// addAddr adds the address
-func (hostname *avahiHostname) addAddr(addr netip.Addr) {
-	hostname.addrs[addr] = struct{}{}
-}
-
-// delAddr deletes the address
-func (hostname *avahiHostname) delAddr(addr netip.Addr) {
-	delete(hostname.addrs, addr)
-}
-
-// avahiHostnameKey identifies a particular instance of
-// the avahiHostname
-type avahiHostnameKey struct {
-	IfIdx    avahi.IfIndex  // Network interface index
-	Proto    avahi.Protocol // Network protocol
-	Hostname string         // Hostname string (FQDN)
-}
-
-// String returns string representation of the avahiHostnameKey,
-// for debugging.
-func (key avahiHostnameKey) String() string {
-	var ifname string
-
-	if ifi, err := net.InterfaceByIndex(int(key.IfIdx)); err == nil {
-		ifname = ifi.Name
-	} else {
-		ifname = fmt.Sprintf("%d", key.IfIdx)
-	}
-
-	return fmt.Sprintf("%q (%s,%s)", key.Hostname, key.Proto, ifname)
-}
-
-// avahiHostnameKeyFromRecordBrowserEvent makes avahiHostnameKey
-// from the avahi.RecordBrowserEvent
-func avahiHostnameKeyFromRecordBrowserEvent(
-	evnt *avahi.RecordBrowserEvent) avahiHostnameKey {
-
-	return avahiHostnameKey{
-		IfIdx:    evnt.IfIdx,
-		Proto:    evnt.Proto,
-		Hostname: evnt.Name,
-	}
 }
---
+++ discovery/dnssd/glue_avahi.go
@@ -0,0 +1,185 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// DNS-SD service discovery
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Avahi glue
+
+package dnssd
+
+import (
+	"fmt"
+	"net"
+	"net/netip"
+	"time"
+
+	"github.com/alexpevzner/go-avahi"
+)
+
+// Parameters:
+const (
+	// Avahi client may fail to start if avahi-daemon is not
+	// running for for the similar reasons.
+	//
+	// If failed, Avahi client will automatically restart
+	// with the following interval between attempts.
+	avahiClientRestartInterval = 1 * time.Second
+)
+
+// avahiLookupFlagsTable contains mapping of LookupFlags to avahi.LookupFlags
+var avahiLookupFlagsTable = [...]avahi.LookupFlags{
+	0:               avahi.LookupUseMulticast,
+	LookupClassical: avahi.LookupUseWideArea,
+	LookupMulticast: avahi.LookupUseMulticast,
+	LookupBoths:     avahi.LookupUseMulticast,
+}
+
+// avahiLookupFlags maps LookupFlags to avahi.LookupFlags
+func avahiLookupFlags(flags LookupFlags) avahi.LookupFlags {
+	return avahiLookupFlagsTable[flags&LookupBoths]
+}
+
+// avahiService is the per-service-instance structure
+// that manages resources associated with the service
+type avahiService struct {
+	key         avahiServiceKey        // Identity
+	svcResolver *avahi.ServiceResolver // Service resolver
+	txtBrowser  *avahi.RecordBrowser   // TXT record resolver
+	port        uint16                 // IP port
+	hostname    *avahiHostname         // Hostname resolver
+}
+
+// avahiServiceKey identifies a particular instance of
+// service of the particular type.
+type avahiServiceKey struct {
+	IfIdx        avahi.IfIndex  // Network interface index
+	Proto        avahi.Protocol // Network protocol
+	InstanceName string         // Service instance name
+	SvcType      string         // Service type
+	Domain       string         // Service domain
+}
+
+// FQDN returns the full-qualified domain name for the
+// service instance.
+func (key avahiServiceKey) FQDN() string {
+	return avahi.DomainServiceNameJoin(key.InstanceName,
+		key.SvcType, key.Domain)
+}
+
+// String returns string representation of the avahiServiceKey,
+// for debugging.
+func (key avahiServiceKey) String() string {
+	var ifname string
+
+	if ifi, err := net.InterfaceByIndex(int(key.IfIdx)); err == nil {
+		ifname = ifi.Name
+	} else {
+		ifname = fmt.Sprintf("%d", key.IfIdx)
+	}
+
+	return fmt.Sprintf("%q (%s,%s)", key.FQDN(), key.Proto, ifname)
+}
+
+// avahiServiceKeyFromServiceBrowserEvent makes avahiServiceKey
+// from the avahi.ServiceBrowserEvent.
+func avahiServiceKeyFromServiceBrowserEvent(
+	evnt *avahi.ServiceBrowserEvent) avahiServiceKey {
+
+	return avahiServiceKey{
+		IfIdx:        evnt.IfIdx,
+		Proto:        evnt.Proto,
+		InstanceName: evnt.InstanceName,
+		SvcType:      evnt.SvcType,
+		Domain:       evnt.Domain,
+	}
+}
+
+// avahiServiceKeyFromResolverEvent makes avahiServiceKey
+// from the avahi.ServiceResolverEvent.
+func avahiServiceKeyFromResolverEvent(
+	evnt *avahi.ServiceResolverEvent) avahiServiceKey {
+
+	return avahiServiceKey{
+		IfIdx:        evnt.IfIdx,
+		Proto:        evnt.Proto,
+		InstanceName: evnt.InstanceName,
+		SvcType:      evnt.SvcType,
+		Domain:       evnt.Domain,
+	}
+}
+
+// avahiServiceKeyFromRecordBrowserEvent makes avahiServiceKey
+// from the avahi.RecordBrowserEvent
+func avahiServiceKeyFromRecordBrowserEvent(
+	evnt *avahi.RecordBrowserEvent) avahiServiceKey {
+
+	instance, svctype, domain := avahi.DomainServiceNameSplit(evnt.Name)
+
+	return avahiServiceKey{
+		IfIdx:        evnt.IfIdx,
+		Proto:        evnt.Proto,
+		InstanceName: instance,
+		SvcType:      svctype,
+		Domain:       domain,
+	}
+}
+
+// avahiHostname is the per-hostname structure that manages
+// resources associated with the hostname
+type avahiHostname struct {
+	key         avahiHostnameKey           // Identity
+	addrBrowser *avahi.RecordBrowser       // A/AAAA record resolver
+	addrs       map[netip.Addr]struct{}    // Resolved addresses
+	services    map[*avahiService]struct{} // Dependent services
+}
+
+// hasAddr reports if address already known
+func (hostname *avahiHostname) hasAddr(addr netip.Addr) bool {
+	_, found := hostname.addrs[addr]
+	return found
+}
+
+// addAddr adds the address
+func (hostname *avahiHostname) addAddr(addr netip.Addr) {
+	hostname.addrs[addr] = struct{}{}
+}
+
+// delAddr deletes the address
+func (hostname *avahiHostname) delAddr(addr netip.Addr) {
+	delete(hostname.addrs, addr)
+}
+
+// avahiHostnameKey identifies a particular instance of
+// the avahiHostname
+type avahiHostnameKey struct {
+	IfIdx    avahi.IfIndex  // Network interface index
+	Proto    avahi.Protocol // Network protocol
+	Hostname string         // Hostname string (FQDN)
+}
+
+// String returns string representation of the avahiHostnameKey,
+// for debugging.
+func (key avahiHostnameKey) String() string {
+	var ifname string
+
+	if ifi, err := net.InterfaceByIndex(int(key.IfIdx)); err == nil {
+		ifname = ifi.Name
+	} else {
+		ifname = fmt.Sprintf("%d", key.IfIdx)
+	}
+
+	return fmt.Sprintf("%q (%s,%s)", key.Hostname, key.Proto, ifname)
+}
+
+// avahiHostnameKeyFromRecordBrowserEvent makes avahiHostnameKey
+// from the avahi.RecordBrowserEvent
+func avahiHostnameKeyFromRecordBrowserEvent(
+	evnt *avahi.RecordBrowserEvent) avahiHostnameKey {
+
+	return avahiHostnameKey{
+		IfIdx:    evnt.IfIdx,
+		Proto:    evnt.Proto,
+		Hostname: evnt.Name,
+	}
+}
