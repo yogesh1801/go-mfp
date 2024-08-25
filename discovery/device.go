f9938a66c360d3571bf401d25dd9381a480b7376
Alexander Pevzner <pzz@apevzner.com>
2024-08-25 03:31:23 +0300

    discovery: DeviceID definition updated

--- discovery/device.go
+++ discovery/device.go
@@ -10,12 +10,10 @@ package discovery
 
 import "github.com/alexpevzner/mfp/uuid"
 
-// DeviceID contains information that identifies a device.
+// DeviceID contains combination of parameters that identifies a device.
 //
 // Please note, depending on a discovery protocol being used, not
 // all the fields of the following structure will have any sense.
-// And although this package attempts to be protocol-neutral, this
-// fact is hard to ignore here.
 //
 // Note also, that device UUID is not necessary the same between
 // protocols. Some Canon devices known to use different UUID for
@@ -26,12 +24,19 @@ import "github.com/alexpevzner/mfp/uuid"
 //	DeviceName - unique device name, in the DNS-SD sence.
 //	             E.g., "Kyocera ECOSYS M2040dn",
 //	UUID       - device UUID
+//	Realm      - search realm. Different realms are treated as
+//	             independent namespaces.
+//	SubRealm   - allows backend to further divide its namespace
+//	             (for example, to split it between IP4/IP6)
+//	Kind       - specifies device kind (e.g., "IPP printer")
+//	Serial     - device serial number, if appropriate (i.e., for USB)
+//	MakeModel  - device make and model (e.g., "HP DeskJet 2540")
 type DeviceID struct {
 	DeviceName string      // Realm-unique device name
-	Realm      DeviceRealm // Search realm
-	IfIdx      int         // For multicast-based network discovery
-	Kind       DeviceKind  // Kind of device
 	UUID       uuid.UUID   // uuid.NilUUID if not available
+	Realm      DeviceRealm // Search realm
+	SubRealm   string      // Backend-specific subrealm
+	Kind       DeviceKind  // Kind of device
 	Serial     string      // "" if not avaliable
 	MakeModel  string      // Just for user information
 }
