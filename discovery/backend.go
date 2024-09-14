033565c7e32bffa79faa224448fcb22189e2c079
Alexander Pevzner <pzz@apevzner.com>
2024-09-14 19:15:13 +0300

    discovery: documentation updated

--- discovery/backend.go
+++ discovery/backend.go
@@ -10,6 +10,23 @@ package discovery
 
 // Backend scans/monitors its search [Realm] and reports discovered
 // devices by sending series of [Event] into the provided [Eventqueue].
+//
+// The following model of operation is assumed:
+//   - The search realm, where Backend operates (local network, for example)
+//     contains some connected devices.
+//   - Each device contains one or more units, and each unit may be
+//     either print unit or scan unit
+//   - Device may expose multiple interfaces to the same physical
+//     unit. For example, printer may support multiple protocols (say,
+//     IPP and LPD). Different interfaces to the same physical unit
+//     needs to be reported as different units.
+//   - Even the same interface may be visible to Backend as one or
+//     more distinct units. For example, the same printer may be visible
+//     via WiFi and via the Ethernet connection. This is up to Backend,
+//     either to merge these "virtual units" together, by reporting
+//     them as a single unit with combined endpoints, or report them
+//     separately. At the later case, Backend should use UnitID.SubRealm
+//     to distinguish between these virtual units.
 type Backend interface {
 	// Name returns backend name.
 	Name() string
--- discovery/device.go
+++ discovery/device.go
@@ -15,16 +15,6 @@ package discovery
 //
 // Each unit has its unique [UnitID], the combination of parameters,
 // that uniquely identifies the unit.
-//
-// If, due to the peculiarities of the search protocol, the same device
-// can appear as several different ones, this is at the search [Backend]
-// discretion, either to merge these multiple instances by itself or to
-// leave this work up to the discovery system.
-//
-// If Backend decides to merge by itself, the resulting unit should appear
-// as a single unit with merged endpoints. Otherwise, each appearance should
-// appear as distinct unit (units with distinct UnitID), and discovery
-// subsystem will merge them, if UnitUDs is "similar enough".
 type Device struct {
 	PrintUnits []PrintUnit
 	ScanUnits  []ScanUnit
