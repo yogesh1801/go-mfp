dca1c4513037f5d2a0fde9bf78daa02c5515ec95
Alexander Pevzner <pzz@apevzner.com>
2024-09-18 11:46:24 +0300

    discovery/dnssd: discovery.EventMetadata event generation

--- discovery/dnssd/unit.go
+++ discovery/dnssd/unit.go
@@ -42,6 +42,7 @@ func newPrinterUnit(queue *discovery.Eventqueue,
 	}
 
 	un.queue.Push(&discovery.EventAddUnit{ID: un.id})
+	un.queue.Push(&discovery.EventMetadata{ID: un.id, Meta: txt.meta})
 	un.queue.Push(&discovery.EventPrinterParameters{
 		ID:      un.id,
 		Printer: *txt.params,
@@ -63,6 +64,7 @@ func newScannerUnit(queue *discovery.Eventqueue,
 	}
 
 	un.queue.Push(&discovery.EventAddUnit{ID: un.id})
+	un.queue.Push(&discovery.EventMetadata{ID: un.id, Meta: txt.meta})
 	un.queue.Push(&discovery.EventScannerParameters{
 		ID:      un.id,
 		Scanner: *txt.params,
