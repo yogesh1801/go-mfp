bea991c9b10e7a9245d552bb18676ff3392178ff
Alexander Pevzner <pzz@apevzner.com>
2024-09-18 16:39:32 +0300

    discovery: initial implementation of the discovery cache

---
+++ discovery/cache.go
@@ -0,0 +1,193 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// Device discovery
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Discovery cache
+
+package discovery
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// cache represents the discovery cache
+type cache struct {
+	ctime time.Time             // Cache creation time
+	units map[UnitID]*cacheUnit // Cached units
+}
+
+// cacheUnit is the cache entry for print/scan/faxout units.
+type cacheUnit struct {
+	id               UnitID    // Unit identity
+	meta             Metadata  // Unit metadata
+	params           any       // PrinterParameters or ScannerParameters
+	hasMeta          bool      // Metadata is received
+	hasParams        bool      // Parameters are received
+	endpoints        []string  // Unit endpoints
+	stagingEndpoints []string  // Newly discovered endpoints, on quarantine
+	stagingDoneAt    time.Time // End of staging time
+}
+
+// newCache creates the new discovery cache
+func newCache() *cache {
+	return &cache{
+		ctime: time.Now(),
+		units: make(map[UnitID]*cacheUnit),
+	}
+}
+
+// AddUnit adds new unit. Called when EventAddUnit is received.
+func (c *cache) AddUnit(id UnitID) error {
+	if c.units[id] != nil {
+		return errors.New("unit already added")
+	}
+
+	c.units[id] = &cacheUnit{id: id}
+	return nil
+}
+
+// DelUnit deletes the unit. Called when EventDelUnit is received.
+func (c *cache) DelUnit(id UnitID) error {
+	if c.units[id] == nil {
+		return errors.New("unknown UnitID")
+	}
+
+	delete(c.units, id)
+	return nil
+}
+
+// SetMetadata saves unit Metadata.
+// Called when EventMetadata is received
+func (c *cache) SetMetadata(id UnitID, meta Metadata) error {
+	un := c.units[id]
+	if un == nil {
+		return errors.New("unknown UnitID")
+	}
+
+	un.meta = meta
+	un.hasMeta = true
+
+	return nil
+}
+
+// SetPrinterParameters saves printer parameters.
+// Called when EventPrinterParameters is received
+func (c *cache) SetPrinterParameters(id UnitID, p PrinterParameters) error {
+	return c.setParameters(id, ServicePrinter, p)
+}
+
+// SetScannerParameters saves scanner parameters.
+// Called when EventScannerParameters is received
+func (c *cache) SetScannerParameters(id UnitID, p ScannerParameters) error {
+	return c.setParameters(id, ServiceScanner, p)
+}
+
+// AddEndpoint adds unit endpoint.
+func (c *cache) AddEndpoint(id UnitID, endpoint string) error {
+	un := c.units[id]
+	if un == nil {
+		return errors.New("unknown UnitID")
+	}
+
+	if endpointsContain(un.endpoints, endpoint) ||
+		endpointsContain(un.stagingEndpoints, endpoint) {
+		return errors.New("endpoint already added")
+	}
+
+	un.stagingBegin()
+	un.stagingEndpoints, _ = endpointsAdd(un.stagingEndpoints, endpoint)
+
+	return nil
+}
+
+// AddEndpoint deletes unit endpoint.
+func (c *cache) DelEndpoint(id UnitID, endpoint string) error {
+	un := c.units[id]
+	if un == nil {
+		return errors.New("unknown UnitID")
+	}
+
+	switch {
+	case endpointsContain(un.endpoints, endpoint):
+		un.endpoints, _ = endpointsDel(un.endpoints, endpoint)
+
+	case endpointsContain(un.stagingEndpoints, endpoint):
+		un.stagingEndpoints, _ = endpointsDel(un.stagingEndpoints,
+			endpoint)
+	default:
+		return errors.New("unknown endpoint")
+	}
+
+	return nil
+}
+
+// SetFaxoutParameters saves faxout parameters.
+// Called when EventFaxoutParameters is received.
+//
+// Note, Faxout parameters are represented by the PrinterParameters type.
+func (c *cache) SetFaxoutParameters(id UnitID, p PrinterParameters) error {
+	return c.setParameters(id, ServiceFaxout, p)
+}
+
+// setParameters saves unit parameters.
+func (c *cache) setParameters(id UnitID, svcMustBe ServiceType, p any) error {
+	un := c.units[id]
+	if un == nil {
+		return errors.New("unknown UnitID")
+	}
+
+	if un.id.SvcType != svcMustBe {
+		return fmt.Errorf("unit is %s, must be %s",
+			un.id.SvcType, svcMustBe)
+	}
+
+	un.params = p
+	un.hasParams = true
+
+	return nil
+}
+
+// stagingBegin starts staging interval for discovered endpoints.
+//
+// The purpose of this interval is to merge together endpoints
+// discovery events, which may come not in the same time (for example,
+// if device has IP4 and IP6 addresses, these addresses may come
+// spread out over time).
+//
+// So when new endpoint is discovered, we don't publish it immediately,
+// but instead save into the staging area and start the timer. The subsequent
+// endpoints are added to the staging area, but already running timer is
+// not restarted. When the timer expires, all endpoints collected at
+// the staging area is published.
+func (un *cacheUnit) stagingBegin() {
+	if !un.stagingCheck() {
+		un.stagingDoneAt = time.Now().Add(time.Second)
+	}
+}
+
+// stagingEnd publishes all endpoints collected in the staging area.
+func (un *cacheUnit) stagingEnd() {
+	un.endpoints = endpointsMerge(un.endpoints, un.stagingEndpoints)
+	un.stagingEndpoints = un.stagingEndpoints[:]
+}
+
+// stagingCheck checks if staging interval is expired. If so,
+// it merges publishes the staged endpoints and clears staging area.
+//
+// It returns 'true' if the previously started staging interval still
+// in progress.
+func (un *cacheUnit) stagingCheck() bool {
+	switch {
+	case len(un.stagingEndpoints) == 0:
+		return false
+	case un.stagingDoneAt.After(time.Now()):
+		un.stagingEnd()
+		return false
+	}
+
+	return true
+}
---
+++ discovery/endpoints.go
@@ -0,0 +1,106 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// Device discovery
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Functions for management collections of endpoints
+
+package discovery
+
+import "sort"
+
+// endpointsCmp compares two endpoint strings for sorting and searching.
+func endpointsCmp(e1, e2 string) int {
+	switch {
+	case e1 < e2:
+		return -1
+	case e1 > e2:
+		return 1
+	}
+	return 0
+}
+
+// endpointsContain reports if collection of endpoints contains
+// the specified endpoint.
+//
+// endpoints assumed to be sorted.
+func endpointsContain(endpoints []string, endpoint string) bool {
+	_, found := sort.Find(len(endpoints), func(i int) int {
+		return endpointsCmp(endpoint, endpoints[i])
+	})
+	return found
+}
+
+// endpointsAdd adds endpoint into the collection of endpoints.
+//
+// It assumes sorted endpoints on input and returns updated and
+// properly sorted collection on output. It works by modifying
+// the input collection in-place, so be aware.
+//
+// Additionally, it returns a bool flag that indicates that
+// endpoint was actually added.
+func endpointsAdd(endpoints []string, endpoint string) ([]string, bool) {
+	i, found := sort.Find(len(endpoints), func(i int) int {
+		return endpointsCmp(endpoint, endpoints[i])
+	})
+
+	if found {
+		return endpoints, false
+	}
+
+	endpoints = append(endpoints, "")
+	copy(endpoints[i+1:], endpoints[i:])
+	endpoints[i] = endpoint
+
+	return endpoints, true
+}
+
+// endpointsDel deletes endpoint from the collection of endpoints.
+//
+// It assumes sorted endpoints on input and returns updated and
+// properly sorted collection on output. It works by modifying
+// the input collection in-place, so be aware.
+//
+// Additionally, it returns a bool flag that indicates that
+// endpoint was actually found in the collection and deleted.
+func endpointsDel(endpoints []string, endpoint string) ([]string, bool) {
+	i, found := sort.Find(len(endpoints), func(i int) int {
+		return endpointsCmp(endpoint, endpoints[i])
+	})
+
+	if !found {
+		return endpoints, false
+	}
+
+	copy(endpoints[i:], endpoints[i+1:])
+	endpoints = endpoints[:len(endpoints)-1]
+
+	return endpoints, true
+}
+
+// endpointsMerge merges two collections of endpoints.
+//
+// Input collections must be sorted and returned collection
+// is sorted as well.
+func endpointsMerge(endpoints1, endpoints2 []string) []string {
+	out := make([]string, 0, len(endpoints1)+len(endpoints2))
+
+	for len(endpoints1) > 0 && len(endpoints2) > 0 {
+		cmp := endpointsCmp(endpoints1[0], endpoints2[0])
+		switch {
+		case cmp < 0:
+			out = append(out, endpoints1[0])
+			endpoints1 = endpoints1[1:]
+		case cmp > 0:
+			out = append(out, endpoints2[0])
+			endpoints2 = endpoints2[1:]
+		default:
+			out = append(out, endpoints1[0])
+			endpoints1 = endpoints1[1:]
+			endpoints2 = endpoints2[1:]
+		}
+	}
+
+	return out
+}
