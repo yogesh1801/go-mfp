b0978c0dab0f6b11f09f021dfc3145038892a512
Alexander Pevzner <pzz@apevzner.com>
2024-09-30 22:46:01 +0300

    discovery: added ColorMode.String

--- discovery/colormode.go
+++ discovery/colormode.go
@@ -8,6 +8,8 @@
 
 package discovery
 
+import "strings"
+
 // ColorMode defines the color space capabilities of printer or scanner.
 type ColorMode int
 
@@ -18,3 +20,23 @@ const (
 	ColorRGB                             // RGB color
 	ColorBW                              // 1-bit monochrome
 )
+
+// String formats ColorMode as string, for printing and logging
+func (cm ColorMode) String() string {
+	s := []string{}
+
+	if cm&ColorOther != 0 {
+		s = append(s, "other")
+	}
+	if cm&ColorGrayscale != 0 {
+		s = append(s, "grayscale")
+	}
+	if cm&ColorRGB != 0 {
+		s = append(s, "RGB")
+	}
+	if cm&ColorBW != 0 {
+		s = append(s, "BW")
+	}
+
+	return strings.Join(s, ",")
+}
