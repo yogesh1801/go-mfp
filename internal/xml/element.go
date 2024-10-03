dc71d34bd6a1d002c37e4d5827234e46f56977cb
Alexander Pevzner <pzz@apevzner.com>
2024-10-03 13:00:30 +0300

    internal/xml: added Element.Expand

--- internal/xml/element.go
+++ internal/xml/element.go
@@ -8,6 +8,11 @@
 
 package xml
 
+import (
+	"os"
+	"slices"
+)
+
 // Element represents a single decoded XML Element
 type Element struct {
 	Name     string    // Name of this element (ns:name)
@@ -20,4 +25,26 @@ type Element struct {
 type Attr struct {
 	Name  string // Attribute name
 	Value string // Attribute value
+}
+
+// Expand replaces ${var} or $var in the XML [Element.Text] and
+// [Attr.Value] of the entire XML tree based on the mapping
+// function and returns rewritten XML tree.
+//
+// It is uses [os.Expand] function for strings substitution.
+func (root Element) Expand(mapping func(string) string) Element {
+	root.Text = os.Expand(root.Text, mapping)
+	root.Attrs = slices.Clone(root.Attrs)
+	root.Children = slices.Clone(root.Children)
+
+	for i := range root.Attrs {
+		attr := &root.Attrs[i]
+		attr.Value = os.Expand(attr.Value, mapping)
+	}
+
+	for i := range root.Children {
+		root.Children[i] = root.Children[i].Expand(mapping)
+	}
+
+	return root
 }
---
+++ internal/xml/expand_test.go
@@ -0,0 +1,79 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// XML mini library
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Element.Expand test
+
+package xml
+
+import (
+	"reflect"
+	"testing"
+)
+
+// TestExpand tests Element.Expand function
+func TestExpand(t *testing.T) {
+	in := Element{
+		Name: "root",
+		Text: "expanded $ROOT",
+		Attrs: []Attr{
+			{"attr1", "expanded $ATTR1"},
+			{"attr2", "expanded ${ATTR2}"},
+		},
+		Children: []Element{
+			{
+				Name: "child-1",
+				Text: "expanded $CHILD1",
+			},
+			{
+				Name: "child-2",
+				Text: "expanded $CHILD2",
+			},
+		},
+	}
+
+	expected := Element{
+		Name: "root",
+		Text: "expanded root value",
+		Attrs: []Attr{
+			{"attr1", "expanded attr1 value"},
+			{"attr2", "expanded attr2 value"},
+		},
+		Children: []Element{
+			{
+				Name: "child-1",
+				Text: "expanded child1 value",
+			},
+			{
+				Name: "child-2",
+				Text: "expanded child2 value",
+			},
+		},
+	}
+
+	out := in.Expand(
+		func(in string) string {
+			switch in {
+			case "ROOT":
+				return "root value"
+			case "ATTR1":
+				return "attr1 value"
+			case "ATTR2":
+				return "attr2 value"
+			case "CHILD1":
+				return "child1 value"
+			case "CHILD2":
+				return "child2 value"
+			}
+
+			return ""
+		})
+
+	if !reflect.DeepEqual(out, expected) {
+		t.Errorf("expected:\n%s\npresent:\n%s\n",
+			expected.EncodeIndentString(nil, "  "),
+			out.EncodeIndentString(nil, "  "))
+	}
+}
