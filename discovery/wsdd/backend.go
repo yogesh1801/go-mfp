14ced1416a22e87c3611e4d6ec516d7131faaa5a
Alexander Pevzner <pzz@apevzner.com>
2024-10-06 13:16:06 +0300

    discovery/wsdd: initial implementation of UDP multicasts reception

--- discovery/wsdd/backend.go
+++ discovery/wsdd/backend.go
@@ -11,6 +11,7 @@ package wsdd
 import (
 	"context"
 	"sync"
+	"sync/atomic"
 
 	"github.com/alexpevzner/mfp/discovery"
 	"github.com/alexpevzner/mfp/discovery/netstate"
@@ -19,13 +20,14 @@ import (
 
 // backend is the [discovery.Backend] for WSD device discovery.
 type backend struct {
-	ctx    context.Context       // For logging and backend.Close
-	cancel context.CancelFunc    // Context's cancel function
-	queue  *discovery.Eventqueue // Event queue
-	netmon *netstate.Notifier    // Network state monitor
-	mconn4 *mconn                // IP4 multicasts reception connection
-	mconn6 *mconn                // IP6 multicasts reception connection
-	done   sync.WaitGroup        // For backend.Close synchronization
+	ctx     context.Context       // For logging and backend.Close
+	cancel  context.CancelFunc    // Context's cancel function
+	queue   *discovery.Eventqueue // Event queue
+	netmon  *netstate.Notifier    // Network state monitor
+	mconn4  *mconn                // IP4 multicasts reception connection
+	mconn6  *mconn                // IP6 multicasts reception connection
+	closing atomic.Bool           // Close in progress
+	done    sync.WaitGroup        // For backend.Close synchronization
 }
 
 // NewBackend creates a new [discovery.Backend] for WSD device discovery.
@@ -68,21 +70,27 @@ func (back *backend) Name() string {
 func (back *backend) Start(queue *discovery.Eventqueue) {
 	back.queue = queue
 
-	back.done.Add(1)
-	go back.netmonproc()
+	back.done.Add(3)
+
+	go back.netmonProc()
+	go back.mconnProc(back.mconn4)
+	go back.mconnProc(back.mconn6)
 
 	log.Debug(back.ctx, "backend started")
 }
 
 // Close closes the backend
 func (back *backend) Close() {
+	back.closing.Store(true)
 	back.cancel()
+	back.mconn4.Close()
+	back.mconn6.Close()
 	back.done.Wait()
 }
 
 // netmonproc processes netstate.Notifier events.
 // It runs on its own goroutine.
-func (back *backend) netmonproc() {
+func (back *backend) netmonProc() {
 	defer back.done.Done()
 
 	for {
@@ -92,5 +100,28 @@ func (back *backend) netmonproc() {
 		}
 
 		log.Debug(back.ctx, "%s", evnt)
+	}
+}
+
+// mconn4proc receives UDP multicast messages from the multicast conection.
+// the back.mconn4 connection.
+func (back *backend) mconnProc(mc *mconn) {
+	defer back.done.Done()
+
+	for {
+		var buf [65536]byte
+		n, from, cmsg, err := mc.RecvFrom(buf[:])
+
+		if back.closing.Load() {
+			return
+		}
+
+		if err != nil {
+			log.Error(back.ctx, "UDP recv: %s", err)
+			return
+		}
+
+		log.Debug(back.ctx, "%d bytes received from %s%%%d",
+			n, from, cmsg.IfIndex)
 	}
 }
