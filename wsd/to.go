204c9c2f5ec87fcf712fb1271f83b21fbd7f8dbb
Alexander Pevzner <pzz@apevzner.com>
2024-10-09 20:55:21 +0300

    wsd: Added well-known WSD destinations

---
+++ wsd/to.go
@@ -0,0 +1,15 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WSD core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Well-known destinations
+
+package wsd
+
+// Well-known destinations:
+const (
+	ToDiscovery = "urn:schemas-xmlsoap-org:ws:2005:04:discovery"
+	ToAnonymous = "http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous"
+)
