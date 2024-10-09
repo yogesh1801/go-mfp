a66feecd1497cb87865ae92211fac930cf4b1895
Alexander Pevzner <pzz@apevzner.com>
2024-10-09 11:45:14 +0300

    wsd: missed commit

---
+++ wsd/decode.go
@@ -0,0 +1,29 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WSD core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Functions for XML decoding
+
+package wsd
+
+import (
+	"strconv"
+
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// decodeUint64 decodes uint64 from the XML tree
+func decodeUint64(root xmldoc.Element) (v uint64, err error) {
+	v, err = strconv.ParseUint(root.Text, 10, 64)
+	err = xmlErrWrap(root, err)
+	return
+}
+
+// decodeUint64 decodes uint64 from the XML attribute
+func decodeUint64Attr(attr xmldoc.Attr) (v uint64, err error) {
+	v, err = strconv.ParseUint(attr.Value, 10, 64)
+	err = xmlErrWrapAttr(attr, err)
+	return
+}
