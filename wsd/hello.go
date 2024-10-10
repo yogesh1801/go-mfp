46ae189b3de65024f91165a5c8fde92ddc2127e0
Alexander Pevzner <pzz@apevzner.com>
2024-10-10 10:28:41 +0300

    wsd: Hello rewritten on a top of announce

--- wsd/hello.go
+++ wsd/hello.go
@@ -9,9 +9,6 @@
 package wsd
 
 import (
-	"strconv"
-	"strings"
-
 	"github.com/alexpevzner/mfp/xmldoc"
 )
 
@@ -26,40 +23,9 @@ type Hello struct {
 
 // DecodeHello decodes [Hello] from the XML tree
 func DecodeHello(root xmldoc.Element) (hello Hello, err error) {
-	defer func() { err = xmlErrWrap(root, err) }()
-
-	// Lookup message elements
-	EndpointReference := xmldoc.Lookup{
-		Name: NsAddressing + ":EndpointReference", Required: true}
-	Types := xmldoc.Lookup{
-		Name: NsDiscovery + ":" + "Types"}
-	XAddrs := xmldoc.Lookup{
-		Name: NsDiscovery + ":" + "XAddrs"}
-	MetadataVersion := xmldoc.Lookup{
-		Name: NsDiscovery + ":" + "MetadataVersion", Required: true}
-
-	missed := root.Lookup(&EndpointReference, &Types,
-		&XAddrs, &MetadataVersion)
-
-	if missed != nil {
-		err = xmlErrMissed(missed.Name)
-		return
-	}
-
-	// Decode elements
-	hello.EndpointReference, err = DecodeEndpointReference(
-		EndpointReference.Elem)
-
-	if err == nil && Types.Found {
-		hello.Types = strings.Fields(Types.Elem.Text)
-	}
-
-	if err == nil && XAddrs.Found {
-		hello.XAddrs, err = DecodeXAddrs(XAddrs.Elem)
-	}
-
+	ann, err := decodeAnnounce(root)
 	if err == nil {
-		hello.MetadataVersion, err = decodeUint64(MetadataVersion.Elem)
+		hello = Hello(ann)
 	}
 
 	return
@@ -67,32 +33,7 @@ func DecodeHello(root xmldoc.Element) (hello Hello, err error) {
 
 // ToXML generates XML tree for the message body
 func (hello Hello) ToXML() xmldoc.Element {
-	elm := xmldoc.Element{
-		Name: NsDiscovery + ":" + "Hello",
-		Children: []xmldoc.Element{
-			hello.EndpointReference.ToXML(
-				NsAddressing + ":EndpointReference"),
-			{
-				Name: NsDiscovery + ":" + "MetadataVersion",
-				Text: strconv.FormatUint(hello.MetadataVersion, 10),
-			},
-		},
-	}
-
-	if len(hello.Types) != 0 {
-		chld := xmldoc.Element{
-			Name: NsDiscovery + ":" + "Types",
-			Text: strings.Join(hello.Types, " "),
-		}
-
-		elm.Children = append(elm.Children, chld)
-	}
-
-	if len(hello.XAddrs) != 0 {
-		elm.Children = append(elm.Children, hello.XAddrs.ToXML())
-	}
-
-	return elm
+	return announce(hello).ToXML("Hello")
 }
 
 // MarkUsedNamespace marks [xmldoc.Namespace] entries used by
@@ -101,7 +42,5 @@ func (hello Hello) ToXML() xmldoc.Element {
 // This function should not care about Namespace entries, used
 // by XML tags: they are handled automatically.
 func (hello Hello) MarkUsedNamespace(ns xmldoc.Namespace) {
-	for _, name := range hello.Types {
-		ns.MarkUsedName(name)
-	}
+	announce(hello).MarkUsedNamespace(ns)
 }
