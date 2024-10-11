bc3bb63fb2254a11ceec91ca5af69f19314307f3
Alexander Pevzner <pzz@apevzner.com>
2024-10-11 19:32:51 +0300

    wsd: added ResolveMatches with test

---
+++ wsd/resolvematches.go
@@ -0,0 +1,78 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WSD core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// ResolveMatches message body
+
+package wsd
+
+import (
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// ResolveMatches represents a protocol ResolveMatches message.
+//
+// The matching devices respond with the ResolveMatches to the received
+// [Resolve] solicitation.
+//
+// Please notice, if device matches multiple [Types] it may respond with
+// either separate [ResolveMatch] for each type, of with combined ResolveMatch
+// for all (most, but not all, device prefer the second option).
+type ResolveMatches struct {
+	ResolveMatch []ResolveMatch
+}
+
+// ResolveMatch represents a single Resolve match.
+type ResolveMatch struct {
+	EndpointReference EndpointReference // Stable identifier of the device
+	Types             Types             // Device types
+	XAddrs            XAddrs            // Transport addresses (URLs)
+	MetadataVersion   uint64            // Incremented when metadata changes
+}
+
+// DecodeResolveMatches decodes [ResolveMatches] from the XML tree
+func DecodeResolveMatches(root xmldoc.Element) (rm ResolveMatches, err error) {
+	defer func() { err = xmlErrWrap(root, err) }()
+
+	const name = NsDiscovery + ":ResolveMatch"
+	for _, chld := range root.Children {
+		if chld.Name == name {
+			var ann announce
+			ann, err = decodeAnnounce(chld)
+			if err != nil {
+				return
+			}
+
+			rm.ResolveMatch = append(rm.ResolveMatch, ResolveMatch(ann))
+		}
+	}
+
+	return
+}
+
+// ToXML generates XML tree for the message body
+func (rm ResolveMatches) ToXML() xmldoc.Element {
+	elm := xmldoc.Element{
+		Name: NsDiscovery + ":ResolveMatches",
+	}
+
+	for _, match := range rm.ResolveMatch {
+		chld := announce(match).ToXML("ResolveMatch")
+		elm.Children = append(elm.Children, chld)
+	}
+
+	return elm
+}
+
+// MarkUsedNamespace marks [xmldoc.Namespace] entries used by
+// data elements within the message body, if any.
+//
+// This function should not care about Namespace entries, used
+// by XML tags: they are handled automatically.
+func (rm ResolveMatches) MarkUsedNamespace(ns xmldoc.Namespace) {
+	for _, match := range rm.ResolveMatch {
+		match.Types.MarkUsedNamespace(ns)
+	}
+}
---
+++ wsd/resolvematches_test.go
@@ -0,0 +1,172 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WSD core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// ResolveMatches test
+
+package wsd
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// TestResolveMatches tests ResolveMatches encoding and decoding
+func TestResolveMatches(t *testing.T) {
+	type testData struct {
+		rm     ResolveMatches
+		xml    xmldoc.Element
+		nsused string
+	}
+
+	tests := []testData{
+		{
+			rm: ResolveMatches{
+				ResolveMatch: []ResolveMatch{
+					{
+						EndpointReference: EndpointReference{
+							Address: "urn:uuid:1fccdddc-380e-41df-8d38-b5df20bc47ef",
+						},
+						Types: TypeDevice | TypePrinter | TypeScanner,
+						XAddrs: XAddrs{
+							"http://127.0.0.1/",
+							"https://[::1]/",
+						},
+						MetadataVersion: 1,
+					},
+				},
+			},
+
+			xml: xmldoc.Element{
+				Name: NsDiscovery + ":ResolveMatches",
+				Children: []xmldoc.Element{
+					{
+						Name: NsDiscovery + ":ResolveMatch",
+						Children: []xmldoc.Element{
+							{
+								Name: NsAddressing + ":EndpointReference",
+								Children: []xmldoc.Element{
+									{
+										Name: NsAddressing + ":Address",
+										Text: "urn:uuid:1fccdddc-380e-41df-8d38-b5df20bc47ef",
+									},
+								},
+							},
+							{
+								Name: NsDiscovery + ":MetadataVersion",
+								Text: "1",
+							},
+							{
+								Name: NsDiscovery + ":Types",
+								Text: "" +
+									"devprof:Device " +
+									"print:PrintDeviceType " +
+									"scan:ScanDeviceType",
+							},
+							{
+								Name: NsDiscovery + ":XAddrs",
+								Text: "" +
+									"http://127.0.0.1/ " +
+									"https://[::1]/",
+							},
+						},
+					},
+				},
+			},
+
+			nsused: "devprof,scan,print",
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.rm.ToXML()
+		if !reflect.DeepEqual(xml, test.xml) {
+			t.Errorf("ToXML:\nexpected: %s\npresent:  %s\n",
+				test.xml.EncodeString(NsMap),
+				xml.EncodeString(NsMap))
+		}
+
+		rm, err := DecodeResolveMatches(xml)
+		if err != nil {
+			t.Errorf("DecodeResolveMatches: %s", err)
+			continue
+		}
+
+		if !reflect.DeepEqual(rm, test.rm) {
+			t.Errorf("DecodeResolveMatches:\n"+
+				"expected: %#v\npresent:  %#v\n",
+				test.rm, rm)
+		}
+
+		ns := NsMap.Clone()
+		rm.MarkUsedNamespace(ns)
+
+		nsused := []string{}
+		for _, n := range ns {
+			if n.Used {
+				nsused = append(nsused, n.Prefix)
+			}
+		}
+
+		nsusedPresent := strings.Join(nsused, ",")
+
+		if test.nsused != nsusedPresent {
+			t.Errorf("announce.MarkUsedNamespace:\n"+
+				"expected: %s\n"+
+				"present:  %s\n",
+				test.nsused, nsusedPresent)
+		}
+	}
+}
+
+// TestResolveMatchesDecodeErrors additionally tests ResolveMatches decode errors
+func TestResolveMatchesDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml  xmldoc.Element
+		estr string
+	}
+
+	tests := []testData{
+		{
+			// Empty ResolveMatches is OK
+			xml: xmldoc.Element{
+				Name: NsDiscovery + ":ResolveMatches",
+			},
+		},
+
+		{
+			// But empty ResolveMatch inside is not
+			xml: xmldoc.Element{
+				Name: NsDiscovery + ":ResolveMatches",
+				Children: []xmldoc.Element{
+					{
+						Name:     NsDiscovery + ":ResolveMatch",
+						Children: []xmldoc.Element{},
+					},
+				},
+			},
+
+			estr: "/d:ResolveMatches/d:ResolveMatch/a:EndpointReference: missed",
+		},
+	}
+
+	for _, test := range tests {
+		_, err := DecodeResolveMatches(test.xml)
+		estr := ""
+		if err != nil {
+			estr = err.Error()
+		}
+
+		if estr != test.estr {
+			t.Errorf("%s\nexpected: %q\npresent:  %q",
+				test.xml.EncodeString(NsMap),
+				test.estr, estr)
+		}
+	}
+
+}
