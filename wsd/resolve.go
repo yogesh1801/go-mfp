ba7b489a2a74c9059488c34b8db1ffb390b181a6
Alexander Pevzner <pzz@apevzner.com>
2024-10-11 20:18:50 +0300

    wsd: Resolve added and tested

---
+++ wsd/resolve.go
@@ -0,0 +1,71 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WSD core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Resolve message body
+
+package wsd
+
+import (
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// Resolve represents a protocol Resolve message.
+//
+// This message usually sent as UDP multicast to the
+// 239.255.255.250:3702 or [ff02::c]:3702 in order to solicit
+// devices, that match the [Resolve.EndpointReference] to respond
+// with the [ResolveMatches] message.
+//
+// The typical use case is to obtain XAddrs, if XAddrs are
+// missed in the [Hello] or [ProbeMatches] message.
+type Resolve struct {
+	EndpointReference EndpointReference // Target device
+}
+
+// DecodeResolve decodes [Resolve] from the XML tree
+func DecodeResolve(root xmldoc.Element) (resolve Resolve, err error) {
+	defer func() { err = xmlErrWrap(root, err) }()
+
+	// Lookup message elements
+	EndpointReference := xmldoc.Lookup{
+		Name: NsAddressing + ":EndpointReference", Required: true}
+
+	missed := root.Lookup(&EndpointReference)
+
+	if missed != nil {
+		err = xmlErrMissed(missed.Name)
+		return
+	}
+
+	// Decode elements
+	resolve.EndpointReference, err = DecodeEndpointReference(
+		EndpointReference.Elem)
+
+	return
+}
+
+// ToXML generates XML tree for the message body
+func (resolve Resolve) ToXML() xmldoc.Element {
+	elm := xmldoc.Element{
+		Name: NsDiscovery + ":" + "Resolve",
+		Children: []xmldoc.Element{
+			resolve.EndpointReference.ToXML(
+				NsAddressing + ":EndpointReference"),
+		},
+	}
+
+	return elm
+
+}
+
+// MarkUsedNamespace marks [xmldoc.Namespace] entries used by
+// data elements within the message body, if any.
+//
+// This function should not care about Namespace entries, used
+// by XML tags: they are handled automatically.
+func (resolve Resolve) MarkUsedNamespace(ns xmldoc.Namespace) {
+	// Nothing to mark for Resolve
+}
---
+++ wsd/resolve_test.go
@@ -0,0 +1,143 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WSD core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Resolve test
+
+package wsd
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// TestResolve tests Resolve encoding and decoding
+func TestResolve(t *testing.T) {
+	type testData struct {
+		resolve Resolve
+		xml     xmldoc.Element
+		nsused  string
+	}
+
+	tests := []testData{
+		{
+			resolve: Resolve{
+				EndpointReference: EndpointReference{
+					Address: "urn:uuid:1fccdddc-380e-41df-8d38-b5df20bc47ef",
+				},
+			},
+
+			xml: xmldoc.Element{
+				Name: NsDiscovery + ":Resolve",
+				Children: []xmldoc.Element{
+					{
+						Name: NsAddressing + ":EndpointReference",
+						Children: []xmldoc.Element{
+							{
+								Name: NsAddressing + ":Address",
+								Text: "urn:uuid:1fccdddc-380e-41df-8d38-b5df20bc47ef",
+							},
+						},
+					},
+				},
+			},
+
+			nsused: "",
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.resolve.ToXML()
+		if !reflect.DeepEqual(xml, test.xml) {
+			t.Errorf("ToXML:\nexpected: %s\npresent:  %s\n",
+				test.xml.EncodeString(NsMap),
+				xml.EncodeString(NsMap))
+		}
+
+		resolve, err := DecodeResolve(xml)
+		if err != nil {
+			t.Errorf("DecodeResolve: %s", err)
+			continue
+		}
+
+		if !reflect.DeepEqual(resolve, test.resolve) {
+			t.Errorf("DecodeResolve:\n"+
+				"expected: %#v\npresent:  %#v\n",
+				test.resolve, resolve)
+		}
+
+		ns := NsMap.Clone()
+		resolve.MarkUsedNamespace(ns)
+
+		nsused := []string{}
+		for _, n := range ns {
+			if n.Used {
+				nsused = append(nsused, n.Prefix)
+			}
+		}
+
+		nsusedPresent := strings.Join(nsused, ",")
+
+		if test.nsused != nsusedPresent {
+			t.Errorf("announce.MarkUsedNamespace:\n"+
+				"expected: %s\n"+
+				"present:  %s\n",
+				test.nsused, nsusedPresent)
+		}
+	}
+}
+
+// TestResolveDecodeErrors additionally tests Resolve decoding errors
+func TestResolveDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml  xmldoc.Element
+		estr string
+	}
+
+	tests := []testData{
+		{
+			xml: xmldoc.Element{
+				Name: NsDiscovery + ":Resolve",
+				Children: []xmldoc.Element{
+					{
+						Name: NsAddressing + ":EndpointReference",
+						Children: []xmldoc.Element{
+							{
+								Name: NsAddressing + ":Address",
+								Text: "urn:uuid:1fccdddc-380e-41df-8d38-b5df20bc47ef",
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			xml: xmldoc.Element{
+				Name: NsDiscovery + ":Resolve",
+			},
+
+			estr: "/d:Resolve/a:EndpointReference: missed",
+		},
+	}
+
+	for _, test := range tests {
+		_, err := DecodeResolve(test.xml)
+		estr := ""
+		if err != nil {
+			estr = err.Error()
+		}
+
+		if estr != test.estr {
+			t.Errorf("%s\nexpected: %q\npresent:  %q",
+				test.xml.EncodeString(NsMap),
+				test.estr, estr)
+		}
+	}
+
+}
