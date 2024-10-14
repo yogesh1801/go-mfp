69fdb474e69f63e5bf5c2d91a269bb3115e50f37
Alexander Pevzner <pzz@apevzner.com>
2024-10-14 15:03:57 +0300

    wsd: added RequestBody interface

--- wsd/body.go
+++ wsd/body.go
@@ -33,3 +33,11 @@ type Body interface {
 	// encoding of this body.
 	MarkUsedNamespace(xmldoc.Namespace)
 }
+
+// RequestBody is the [Body], suitable for using in request messages.
+type RequestBody interface {
+	Body
+
+	// FillRequestHeader fills request [Header] for this body.
+	FillRequestHeader(hdr *Header)
+}
