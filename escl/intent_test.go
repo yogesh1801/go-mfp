e4f5cc2cf3812b7c3468ea7f34cc8616a53ea21d
Alexander Pevzner <pzz@apevzner.com>
2025-01-02 13:55:51 +0300

    escl: added Intent test

---
+++ escl/intent_test.go
@@ -0,0 +1,63 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Scan intent test
+
+package escl
+
+import "testing"
+
+// TestIntentString tests Intent.String
+func TestIntentString(t *testing.T) {
+	type testData struct {
+		intent Intent
+		s      string
+	}
+
+	tests := []testData{
+		{Document, "Document"},
+		{TextAndGraphic, "TextAndGraphic"},
+		{Photo, "Photo"},
+		{Preview, "Preview"},
+		{Object, "Object"},
+		{BusinessCard, "BusinessCard"},
+		{-1, "Unknown"},
+	}
+
+	for _, test := range tests {
+		s := test.intent.String()
+		if s != test.s {
+			t.Errorf("%d: extected %q, present %q",
+				int(test.intent), test.s, s)
+		}
+	}
+}
+
+// TestDecodeIntent tests DecodeIntent
+func TestDecodeIntent(t *testing.T) {
+	type testData struct {
+		intent Intent
+		s      string
+	}
+
+	tests := []testData{
+		{Document, "Document"},
+		{TextAndGraphic, "TextAndGraphic"},
+		{Photo, "Photo"},
+		{Preview, "Preview"},
+		{Object, "Object"},
+		{BusinessCard, "BusinessCard"},
+		{UnknownIntent, "XXX"},
+	}
+
+	for _, test := range tests {
+		intent := DecodeIntent(test.s)
+		if intent != test.intent {
+			t.Errorf("%q: extected %q, present %q",
+				test.s, test.intent, intent)
+		}
+	}
+}
