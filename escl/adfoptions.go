52d4435b81a78da6702b248cf362c76d0ccebc61
Alexander Pevzner <pzz@apevzner.com>
2025-01-08 19:55:27 +0300

    escl: AdfOptions and ColorModes reimplemented on a top of generic.Bitset
    This avoids code duplication

--- escl/adfoptions.go
+++ escl/adfoptions.go
@@ -8,50 +8,12 @@
 
 package escl
 
-import "strings"
+import "github.com/alexpevzner/mfp/internal/generic"
 
 // ADFOptions contains a set (bitmask) of [ADFOption]s.
-type ADFOptions int
+type ADFOptions = generic.Bitset[ADFOption]
 
 // MakeADFOptions makes [ADFOptions] from the list of [ADFOption]s.
 func MakeADFOptions(list ...ADFOption) ADFOptions {
-	var opts ADFOptions
-
-	for _, opt := range list {
-		opts.Add(opt)
-	}
-
-	return opts
-}
-
-// String returns a string representation of the [ADFOptions],
-// for debugging.
-func (opts ADFOptions) String() string {
-	modes := [...]ADFOption{DetectPaperLoaded, SelectSinglePage, Duplex}
-	s := make([]string, 0, len(modes))
-
-	for _, opt := range modes {
-		if opts.Contains(opt) {
-			s = append(s, opt.String())
-		}
-	}
-
-	return strings.Join(s, ",")
-}
-
-// Add adds [ADFOption] to the set.
-func (opts *ADFOptions) Add(opt ADFOption) *ADFOptions {
-	*opts |= 1 << opt
-	return opts
-}
-
-// Del deletes [ADFOption] from the set.
-func (opts *ADFOptions) Del(opt ADFOption) *ADFOptions {
-	*opts &^= 1 << opt
-	return opts
-}
-
-// Contains reports if [ADFOption] exists in the set.
-func (opts ADFOptions) Contains(opt ADFOption) bool {
-	return (opts & (1 << opt)) != 0
+	return generic.MakeBitset(list...)
 }
--- escl/colormodes.go
+++ escl/colormodes.go
@@ -8,51 +8,12 @@
 
 package escl
 
-import "strings"
+import "github.com/alexpevzner/mfp/internal/generic"
 
 // ColorModes contains a set (bitmask) of [ColorMode]s.
-type ColorModes int
+type ColorModes = generic.Bitset[ColorMode]
 
 // MakeColorModes makes [ColorModes] from the list of [ColorMode]s.
 func MakeColorModes(list ...ColorMode) ColorModes {
-	var cmodes ColorModes
-
-	for _, cm := range list {
-		cmodes.Add(cm)
-	}
-
-	return cmodes
-}
-
-// String returns a string representation of the [ColorModes],
-// for debugging.
-func (cmodes ColorModes) String() string {
-	modes := [...]ColorMode{BlackAndWhite1, Grayscale8, Grayscale16,
-		RGB24, RGB48}
-	s := make([]string, 0, len(modes))
-
-	for _, cm := range modes {
-		if cmodes.Contains(cm) {
-			s = append(s, cm.String())
-		}
-	}
-
-	return strings.Join(s, ",")
-}
-
-// Add adds [ColorMode] to the set.
-func (cmodes *ColorModes) Add(cm ColorMode) *ColorModes {
-	*cmodes |= 1 << cm
-	return cmodes
-}
-
-// Del deletes [ColorMode] from the set.
-func (cmodes *ColorModes) Del(cm ColorMode) *ColorModes {
-	*cmodes &^= 1 << cm
-	return cmodes
-}
-
-// Contains reports if [ColorMode] exists in the set.
-func (cmodes ColorModes) Contains(cm ColorMode) bool {
-	return (cmodes & (1 << cm)) != 0
+	return generic.MakeBitset(list...)
 }
