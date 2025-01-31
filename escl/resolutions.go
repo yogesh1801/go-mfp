f5d7ce6ee3d48977c7f77a0e361ddfb3271551d4
Alexander Pevzner <pzz@apevzner.com>
2025-01-31 16:40:39 +0300

    escl: XML encoding/decoding of SupportedResolutions and friends

--- escl/resolutions.go
+++ escl/resolutions.go
@@ -8,13 +8,110 @@
 
 package escl
 
-import "github.com/alexpevzner/mfp/optional"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/alexpevzner/mfp/optional"
+	"github.com/alexpevzner/mfp/xmldoc"
+)
 
 // SupportedResolutions defines the set of resolutions,
 // supported by the scanner.
+//
+// It may optionally contain the ColorMode element, if supported resolutions
+// depend on a color mode. At this case, [SettingProfile.SupportedResolutions]
+// will contain one or more entries with specified ColorMode and may also
+// contain a default entry with ColorMode unset.
+//
+// eSCL Technical Specification, 8.1.1.
 type SupportedResolutions struct {
+	ColorMode           optional.Val[ColorMode]       // If depends on color
 	DiscreteResolutions DiscreteResolutions           // Discrete res
 	ResolutionRange     optional.Val[ResolutionRange] // Res range
+}
+
+// decodeSupportedResolutions decodes [SupportedResolutions] from the
+// XML tree.
+func decodeSupportedResolutions(root xmldoc.Element) (
+	supp SupportedResolutions, err error) {
+
+	defer func() { err = xmldoc.XMLErrWrap(root, err) }()
+
+	// Lookup relevant XML elements
+	colormode := xmldoc.Lookup{Name: NsScan + ":ColorMode"}
+	discrete := xmldoc.Lookup{Name: NsScan + ":DiscreteResolutions"}
+	ranges := xmldoc.Lookup{Name: NsScan + ":ResolutionRange"}
+
+	root.Lookup(&colormode, &discrete, &ranges)
+
+	if !discrete.Found && !ranges.Found {
+		err = fmt.Errorf("missed %s and %s", discrete.Name, ranges.Name)
+		return
+	}
+
+	if colormode.Found {
+		var cm ColorMode
+		cm, err = decodeColorMode(colormode.Elem)
+		if err != nil {
+			err = xmldoc.XMLErrWrap(colormode.Elem, err)
+			return
+		}
+
+		supp.ColorMode = optional.New(cm)
+	}
+
+	if discrete.Found {
+		for _, elem := range discrete.Elem.Children {
+			var res DiscreteResolution
+			res, err = decodeDiscreteResolution(elem)
+			if err != nil {
+				return
+			}
+
+			supp.DiscreteResolutions = append(
+				supp.DiscreteResolutions, res)
+		}
+	}
+
+	if ranges.Found {
+		var rng ResolutionRange
+		rng, err = decodeResolutionRange(ranges.Elem)
+		if err != nil {
+			return
+		}
+		supp.ResolutionRange = optional.New(rng)
+	}
+
+	return
+}
+
+// toXML generates XML tree for the [SupportedResolutions].
+func (supp SupportedResolutions) toXML(name string) xmldoc.Element {
+	elm := xmldoc.Element{Name: name}
+
+	if supp.ColorMode != nil {
+		cm := *supp.ColorMode
+		chld := cm.toXML(NsScan + ":ColorMode")
+		elm.Children = append(elm.Children, chld)
+	}
+
+	if supp.DiscreteResolutions != nil {
+		chld := xmldoc.Element{Name: NsScan + ":DiscreteResolutions"}
+		for _, r := range supp.DiscreteResolutions {
+			chld.Children = append(chld.Children,
+				r.toXML(NsScan+":DiscreteResolution"))
+		}
+		elm.Children = append(elm.Children, chld)
+	}
+
+	if supp.ResolutionRange != nil {
+		rng := *supp.ResolutionRange
+		chld := rng.toXML(NsScan + ":ResolutionRange")
+		elm.Children = append(elm.Children, chld)
+	}
+
+	return elm
 }
 
 // DiscreteResolutions define a set of discrete resolutions,
@@ -24,8 +121,44 @@ type DiscreteResolutions []DiscreteResolution
 // ResolutionRange defines a set of resolutions range,
 // supported by the scanner.
 type ResolutionRange struct {
-	XResolutionRange XYResolutionRange // Horizontal range
-	YResolutionRange XYResolutionRange // Vertical range
+	XResolutionRange Range // Horizontal range
+	YResolutionRange Range // Vertical range
+}
+
+// decodeResolutionRange, decodes [ResolutionRange] from the XML tree.
+func decodeResolutionRange(root xmldoc.Element) (
+	rng ResolutionRange, err error) {
+
+	defer func() { err = xmldoc.XMLErrWrap(root, err) }()
+
+	// Lookup relevant XML elements
+	x := xmldoc.Lookup{Name: NsScan + ":XResolutionRange", Required: true}
+	y := xmldoc.Lookup{Name: NsScan + ":YResolutionRange", Required: true}
+
+	missed := root.Lookup(&x, &y)
+	if missed != nil {
+		err = xmldoc.XMLErrMissed(missed.Name)
+		return
+	}
+
+	// Decode elements
+	rng.XResolutionRange, err = decodeRange(x.Elem)
+	if err == nil {
+		rng.YResolutionRange, err = decodeRange(y.Elem)
+	}
+
+	return
+}
+
+// toXML generates XML tree for the [ResolutionRange].
+func (rng ResolutionRange) toXML(name string) xmldoc.Element {
+	x := rng.XResolutionRange.toXML(NsScan + ":XResolutionRange")
+	y := rng.YResolutionRange.toXML(NsScan + ":YResolutionRange")
+
+	return xmldoc.Element{
+		Name:     name,
+		Children: []xmldoc.Element{x, y},
+	}
 }
 
 // DiscreteResolution defines a discrete resolution, supported by the scanner.
@@ -34,6 +167,50 @@ type DiscreteResolution struct {
 	YResolution int // Vertical resolution, DPI
 }
 
-// XYResolutionRange defines a range of horizontal or vertical resolutions,
-// supported by the scanner, in DPI.
-type XYResolutionRange = Range
+// decodeDiscreteResolution decodes [DiscreteResolution] from the
+// XML tree.
+func decodeDiscreteResolution(root xmldoc.Element) (
+	res DiscreteResolution, err error) {
+
+	defer func() { err = xmldoc.XMLErrWrap(root, err) }()
+
+	// Lookup relevant XML elements
+	x := xmldoc.Lookup{Name: NsScan + ":XResolution", Required: true}
+	y := xmldoc.Lookup{Name: NsScan + ":YResolution", Required: true}
+
+	missed := root.Lookup(&x, &y)
+	if missed != nil {
+		err = xmldoc.XMLErrMissed(missed.Name)
+		return
+	}
+
+	// Decode elements
+	res.XResolution, err = decodeNonNegativeInt(x.Elem)
+	if err == nil {
+		res.YResolution, err = decodeNonNegativeInt(y.Elem)
+	}
+
+	return
+}
+
+// toXML generates XML tree for the [DiscreteResolution].
+func (res DiscreteResolution) toXML(name string) xmldoc.Element {
+	x := strconv.FormatUint(uint64(res.XResolution), 10)
+	y := strconv.FormatUint(uint64(res.YResolution), 10)
+
+	elm := xmldoc.Element{
+		Name: name,
+		Children: []xmldoc.Element{
+			{
+				Name: NsScan + ":" + "XResolution",
+				Text: x,
+			},
+			{
+				Name: NsScan + ":" + "YResolution",
+				Text: y,
+			},
+		},
+	}
+
+	return elm
+}
---
+++ escl/resolutions_test.go
@@ -0,0 +1,449 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Scanner resolutions test
+
+package escl
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/alexpevzner/mfp/optional"
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// TestResolutionRange tests [ResolutionRange] to/from the XML conversion
+func TestResolutionRange(t *testing.T) {
+	type testData struct {
+		rng ResolutionRange
+		xml xmldoc.Element
+	}
+
+	tests := []testData{
+		{
+			rng: ResolutionRange{
+				Range{300, 1200, 450, optional.New(150)},
+				Range{100, 600, 300, optional.New(100)},
+			},
+			xml: xmldoc.WithChildren(
+				NsScan+":ResolutionRange",
+				xmldoc.WithChildren(
+					NsScan+":XResolutionRange",
+					xmldoc.WithText(NsScan+":Min", "300"),
+					xmldoc.WithText(NsScan+":Max", "1200"),
+					xmldoc.WithText(NsScan+":Normal", "450"),
+					xmldoc.WithText(NsScan+":Step", "150"),
+				),
+				xmldoc.WithChildren(
+					NsScan+":YResolutionRange",
+					xmldoc.WithText(NsScan+":Min", "100"),
+					xmldoc.WithText(NsScan+":Max", "600"),
+					xmldoc.WithText(NsScan+":Normal", "300"),
+					xmldoc.WithText(NsScan+":Step", "100"),
+				),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.rng.toXML(NsScan + ":ResolutionRange")
+		if !reflect.DeepEqual(xml, test.xml) {
+			t.Errorf("encode mismatch:\n"+
+				"expected: %s\n"+
+				"present:  %s\n",
+				test.xml.EncodeString(nil),
+				xml.EncodeString(nil))
+		}
+
+		rng, err := decodeResolutionRange(test.xml)
+		if err != nil {
+			t.Errorf("decode error:\n"+
+				"input: %s\n"+
+				"error:  %s\n",
+				test.xml.EncodeString(nil), err)
+			continue
+		}
+
+		if !reflect.DeepEqual(rng, test.rng) {
+			t.Errorf("decode mismatch:\n"+
+				"expected: %#v\n"+
+				"present:  %#v\n",
+				test.rng, rng)
+		}
+	}
+}
+
+// TestResolutionRangeDecodeErrors tests [ResolutionRange] XML decode
+// errors handling
+func TestResolutionRangeDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element
+		err string
+	}
+
+	tests := []testData{
+		{
+			xml: xmldoc.WithChildren(
+				NsScan+":ResolutionRange",
+				xmldoc.WithChildren(
+					NsScan+":XResolutionRange",
+					xmldoc.WithText(NsScan+":Min", "300"),
+					xmldoc.WithText(NsScan+":Max", "1200"),
+					xmldoc.WithText(NsScan+":Normal", "450"),
+					xmldoc.WithText(NsScan+":Step", "150"),
+				),
+			),
+			err: `/scan:ResolutionRange/scan:YResolutionRange: missed`,
+		},
+		{
+			xml: xmldoc.WithChildren(
+				NsScan+":ResolutionRange",
+				xmldoc.WithChildren(
+					NsScan+":XResolutionRange",
+					xmldoc.WithText(NsScan+":Min", "300"),
+					xmldoc.WithText(NsScan+":Max", "1200"),
+					xmldoc.WithText(NsScan+":Normal", "450"),
+					xmldoc.WithText(NsScan+":Step", "150"),
+				),
+				xmldoc.WithChildren(
+					NsScan+":YResolutionRange",
+					xmldoc.WithText(NsScan+":Max", "600"),
+					xmldoc.WithText(NsScan+":Normal", "300"),
+					xmldoc.WithText(NsScan+":Step", "100"),
+				),
+			),
+			err: `/scan:ResolutionRange/scan:YResolutionRange/scan:Min: missed`,
+		},
+		{
+			xml: xmldoc.WithChildren(
+				NsScan+":ResolutionRange",
+				xmldoc.WithChildren(
+					NsScan+":XResolutionRange",
+					xmldoc.WithText(NsScan+":Min", "300"),
+					xmldoc.WithText(NsScan+":Max", "1200"),
+					xmldoc.WithText(NsScan+":Normal", "450"),
+					xmldoc.WithText(NsScan+":Step", "150"),
+				),
+				xmldoc.WithChildren(
+					NsScan+":YResolutionRange",
+					xmldoc.WithText(NsScan+":Min", "100"),
+					xmldoc.WithText(NsScan+":Max", "aaa"),
+					xmldoc.WithText(NsScan+":Normal", "300"),
+					xmldoc.WithText(NsScan+":Step", "100"),
+				),
+			),
+			err: `/scan:ResolutionRange/scan:YResolutionRange/scan:Max: invalid int: "aaa"`,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := decodeResolutionRange(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %q\n"+
+				"present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+	}
+}
+
+// TestDiscreteResolution tests [DiscreteResolution] to/from the XML conversion
+func TestDiscreteResolution(t *testing.T) {
+	type testData struct {
+		res DiscreteResolution
+		xml xmldoc.Element
+	}
+
+	tests := []testData{
+		{
+			res: DiscreteResolution{200, 400},
+			xml: xmldoc.WithChildren(
+				NsScan+":DiscreteResolution",
+				xmldoc.WithText(NsScan+":XResolution", "200"),
+				xmldoc.WithText(NsScan+":YResolution", "400"),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.res.toXML(NsScan + ":DiscreteResolution")
+		if !reflect.DeepEqual(xml, test.xml) {
+			t.Errorf("encode mismatch:\n"+
+				"expected: %s\n"+
+				"present:  %s\n",
+				test.xml.EncodeString(nil),
+				xml.EncodeString(nil))
+		}
+
+		res, err := decodeDiscreteResolution(test.xml)
+		if err != nil {
+			t.Errorf("decode error:\n"+
+				"input: %s\n"+
+				"error:  %s\n",
+				test.xml.EncodeString(nil), err)
+			continue
+		}
+
+		if !reflect.DeepEqual(res, test.res) {
+			t.Errorf("decode mismatch:\n"+
+				"expected: %#v\n"+
+				"present:  %#v\n",
+				test.res, res)
+		}
+	}
+}
+
+// TestDiscreteResolutionDecodeErrors tests [DiscreteResolution] XML decode
+// errors handling
+func TestDiscreteResolutionDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element
+		err string
+	}
+
+	tests := []testData{
+		{
+			xml: xmldoc.WithChildren(
+				NsScan+":DiscreteResolution",
+				xmldoc.WithText(NsScan+":YResolution", "400"),
+			),
+			err: `/scan:DiscreteResolution/scan:XResolution: missed`,
+		},
+		{
+			xml: xmldoc.WithChildren(
+				NsScan+":DiscreteResolution",
+				xmldoc.WithText(NsScan+":XResolution", "200"),
+			),
+			err: `/scan:DiscreteResolution/scan:YResolution: missed`,
+		},
+		{
+			xml: xmldoc.WithChildren(
+				NsScan+":DiscreteResolution",
+				xmldoc.WithText(NsScan+":XResolution", "200"),
+				xmldoc.WithText(NsScan+":YResolution", "aaa"),
+			),
+			err: `/scan:DiscreteResolution/scan:YResolution: invalid int: "aaa"`,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := decodeDiscreteResolution(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %q\n"+
+				"present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+	}
+}
+
+// TestSupportedResolutions tests [SupportedResolutions] to/from the XML
+// conversion
+func TestSupportedResolutions(t *testing.T) {
+	type testData struct {
+		supp SupportedResolutions
+		xml  xmldoc.Element
+	}
+
+	tests := []testData{
+		{
+			// DiscreteResolutions + ResolutionRange
+			supp: SupportedResolutions{
+				DiscreteResolutions: []DiscreteResolution{
+					{100, 100},
+					{200, 200},
+				},
+				ResolutionRange: optional.New(ResolutionRange{
+					XResolutionRange: Range{
+						100, 300, 150, nil,
+					},
+					YResolutionRange: Range{
+						100, 300, 150, nil,
+					},
+				}),
+			},
+			xml: xmldoc.WithChildren(
+				NsScan+":SupportedResolutions",
+				xmldoc.WithChildren(
+					NsScan+":DiscreteResolutions",
+					xmldoc.WithChildren(
+						NsScan+":DiscreteResolution",
+						xmldoc.WithText(NsScan+":XResolution", "100"),
+						xmldoc.WithText(NsScan+":YResolution", "100"),
+					),
+					xmldoc.WithChildren(
+						NsScan+":DiscreteResolution",
+						xmldoc.WithText(NsScan+":XResolution", "200"),
+						xmldoc.WithText(NsScan+":YResolution", "200"),
+					),
+				),
+				xmldoc.WithChildren(
+					NsScan+":ResolutionRange",
+					xmldoc.WithChildren(
+						NsScan+":XResolutionRange",
+						xmldoc.WithText(NsScan+":Min", "100"),
+						xmldoc.WithText(NsScan+":Max", "300"),
+						xmldoc.WithText(NsScan+":Normal", "150"),
+					),
+					xmldoc.WithChildren(
+						NsScan+":YResolutionRange",
+						xmldoc.WithText(NsScan+":Min", "100"),
+						xmldoc.WithText(NsScan+":Max", "300"),
+						xmldoc.WithText(NsScan+":Normal", "150"),
+					),
+				),
+			),
+		},
+		{
+			// ColorMode + DiscreteResolutions
+			supp: SupportedResolutions{
+				ColorMode: optional.New(RGB24),
+				DiscreteResolutions: []DiscreteResolution{
+					{100, 100},
+					{200, 200},
+				},
+			},
+			xml: xmldoc.WithChildren(
+				NsScan+":SupportedResolutions",
+				xmldoc.WithText(NsScan+":ColorMode", NsScan+":RGB24"),
+				xmldoc.WithChildren(
+					NsScan+":DiscreteResolutions",
+					xmldoc.WithChildren(
+						NsScan+":DiscreteResolution",
+						xmldoc.WithText(NsScan+":XResolution", "100"),
+						xmldoc.WithText(NsScan+":YResolution", "100"),
+					),
+					xmldoc.WithChildren(
+						NsScan+":DiscreteResolution",
+						xmldoc.WithText(NsScan+":XResolution", "200"),
+						xmldoc.WithText(NsScan+":YResolution", "200"),
+					),
+				),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.supp.toXML(NsScan + ":SupportedResolutions")
+		if !reflect.DeepEqual(xml, test.xml) {
+			t.Errorf("encode mismatch:\n"+
+				"expected: %s\n"+
+				"present:  %s\n",
+				test.xml.EncodeString(nil),
+				xml.EncodeString(nil))
+		}
+
+		supp, err := decodeSupportedResolutions(test.xml)
+		if err != nil {
+			t.Errorf("decode error:\n"+
+				"input: %s\n"+
+				"error:  %s\n",
+				test.xml.EncodeString(nil), err)
+			continue
+		}
+
+		if !reflect.DeepEqual(supp, test.supp) {
+			t.Errorf("decode mismatch:\n"+
+				"expected: %#v\n"+
+				"present:  %#v\n",
+				test.supp, supp)
+		}
+	}
+}
+
+// TestSupportedResolutionsDecodeErrors tests [SupportedResolutions] XML decode
+// errors handling
+func TestSupportedResolutionsDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element
+		err string
+	}
+
+	tests := []testData{
+		{
+			// Missed DiscreteResolutions and ResolutionRange
+			xml: xmldoc.WithChildren(
+				NsScan+":SupportedResolutions",
+				xmldoc.WithText(NsScan+":ColorMode", NsScan+":RGB24"),
+			),
+			err: `/scan:SupportedResolutions: missed scan:DiscreteResolutions and scan:ResolutionRange`,
+		},
+		{
+			// Invalid color mode
+			xml: xmldoc.WithChildren(
+				NsScan+":SupportedResolutions",
+				xmldoc.WithText(NsScan+":ColorMode", "AAA"),
+				xmldoc.WithChildren(
+					NsScan+":DiscreteResolutions",
+					xmldoc.WithChildren(
+						NsScan+":DiscreteResolution",
+						xmldoc.WithText(NsScan+":XResolution", "100"),
+						xmldoc.WithText(NsScan+":YResolution", "100"),
+					),
+				),
+			),
+			err: `/scan:SupportedResolutions/scan:ColorMode/scan:ColorMode: invalid ColorMode: "AAA"`,
+		},
+		{
+			// Error in DiscreteResolutions
+			xml: xmldoc.WithChildren(
+				NsScan+":SupportedResolutions",
+				xmldoc.WithChildren(
+					NsScan+":DiscreteResolutions",
+					xmldoc.WithChildren(
+						NsScan+":DiscreteResolution",
+						xmldoc.WithText(NsScan+":XResolution", "AAA"),
+						xmldoc.WithText(NsScan+":YResolution", "100"),
+					),
+				),
+			),
+			err: `/scan:SupportedResolutions/scan:DiscreteResolution/scan:XResolution: invalid int: "AAA"`,
+		},
+		{
+			// Error in ResolutionRange
+			xml: xmldoc.WithChildren(
+				NsScan+":SupportedResolutions",
+				xmldoc.WithChildren(
+					NsScan+":ResolutionRange",
+					xmldoc.WithChildren(
+						NsScan+":XResolutionRange",
+						xmldoc.WithText(NsScan+":Min", "100"),
+						xmldoc.WithText(NsScan+":Max", "300"),
+						xmldoc.WithText(NsScan+":Normal", "150"),
+					),
+				),
+			),
+			err: `/scan:SupportedResolutions/scan:ResolutionRange/scan:YResolutionRange: missed`,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := decodeSupportedResolutions(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %q\n"+
+				"present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+	}
+}
