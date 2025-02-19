da778f339d9fb81b646a18318bd887b9f59b492c
Alexander Pevzner <pzz@apevzner.com>
2025-02-19 10:47:19 +0300

    escl: SupportedResolutions fully tested

--- escl/settingprofile_test.go
+++ escl/settingprofile_test.go
@@ -9,6 +9,7 @@
 package escl
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 
@@ -151,4 +152,100 @@ func TestSettingProfile(t *testing.T) {
 				test.prof, prof)
 		}
 	}
+}
+
+// TestSettingProfileDecodeErrors tests [SettingProfile] XML decode
+// errors handling
+func TestSettingProfileDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element // Input XML
+		err string         // Expected error
+	}
+
+	res := SupportedResolutions{
+		DiscreteResolutions: []DiscreteResolution{
+			{100, 100},
+			{200, 200},
+		},
+	}
+
+	tests := []testData{
+		// Missed required element
+		{
+			xml: xmldoc.WithChildren(NsScan + ":SettingProfile"),
+			err: `/scan:SettingProfile/scan:SupportedResolutions: missed`,
+		},
+
+		// Error in SupportedResolutions
+		{
+			xml: xmldoc.WithChildren(NsScan+":SettingProfile",
+				xmldoc.WithChildren(NsScan+":SupportedResolutions"),
+			),
+			err: `/scan:SettingProfile/scan:SupportedResolutions: missed scan:DiscreteResolutions and scan:ResolutionRange`,
+		},
+
+		// Error in ColorMode
+		{
+			xml: xmldoc.WithChildren(NsScan+":SettingProfile",
+				xmldoc.WithChildren(NsScan+":ColorModes",
+					xmldoc.WithText(NsScan+":ColorMode",
+						"Unknown"),
+				),
+				res.toXML(NsScan+":SupportedResolutions"),
+			),
+			err: `/scan:SettingProfile/scan:ColorModes/scan:ColorMode: invalid ColorMode: "Unknown"`,
+		},
+
+		// Error in ColorSpaces
+		{
+			xml: xmldoc.WithChildren(NsScan+":SettingProfile",
+				xmldoc.WithChildren(NsScan+":ColorSpaces",
+					xmldoc.WithText(NsScan+":ColorSpace",
+						"Unknown"),
+				),
+				res.toXML(NsScan+":SupportedResolutions"),
+			),
+			err: `/scan:SettingProfile/scan:ColorSpaces/scan:ColorSpace: invalid ColorSpace: "Unknown"`,
+		},
+
+		// Error in CcdChannels
+		{
+			xml: xmldoc.WithChildren(NsScan+":SettingProfile",
+				xmldoc.WithChildren(NsScan+":CcdChannels",
+					xmldoc.WithText(NsScan+":CcdChannel",
+						"Unknown"),
+				),
+				res.toXML(NsScan+":SupportedResolutions"),
+			),
+			err: `/scan:SettingProfile/scan:CcdChannels/scan:CcdChannel: invalid CcdChannel: "Unknown"`,
+		},
+
+		// Error in BinaryRenderings
+		{
+			xml: xmldoc.WithChildren(NsScan+":SettingProfile",
+				xmldoc.WithChildren(NsScan+":BinaryRenderings",
+					xmldoc.WithText(NsScan+":BinaryRendering",
+						"Unknown"),
+				),
+				res.toXML(NsScan+":SupportedResolutions"),
+			),
+			err: `/scan:SettingProfile/scan:BinaryRenderings/scan:BinaryRendering: invalid BinaryRendering: "Unknown"`,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := decodeSettingProfile(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %q\n"+
+				"present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+	}
+
 }
