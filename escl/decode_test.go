14aedae0dd3b532bad7241f18b79f73623d0102c
Alexander Pevzner <pzz@apevzner.com>
2025-02-22 11:30:25 +0300

    escl: tests for decodeNonNegativeInt and decodeBool

---
+++ escl/decode_test.go
@@ -0,0 +1,134 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Tests for the basic XML decoding functions
+
+package escl
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/alexpevzner/mfp/xmldoc"
+)
+
+// TestDecodeNonNegativeInt tests decodeNonNegativeInt
+func TestDecodeNonNegativeInt(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element // Input data
+		out int            // Expected output
+		err string         // Expected error
+	}
+
+	tests := []testData{
+		{
+			xml: xmldoc.WithText("test", "123"),
+			out: 123,
+			err: ``,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "0"),
+			out: 0,
+			err: ``,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "2147483647"),
+			out: 2147483647,
+			err: ``,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "-1"),
+			err: `/test: int out of range: -1`,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "2147483648"),
+			err: `/test: int out of range: 2147483648`,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "bad"),
+			err: `/test: invalid int: "bad"`,
+		},
+	}
+
+	for _, test := range tests {
+		out, err := decodeNonNegativeInt(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:          %s\n"+
+				"error expected: %q\n"+
+				"error present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+
+		if err.Error() == "" && out != test.out {
+			t.Errorf("output mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %v\n"+
+				"present:  %v\n",
+				test.xml.EncodeString(nil), test.out, out)
+		}
+	}
+}
+
+// TestDecodeBool tests decodeBool
+func TestDecodeBool(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element // Input data
+		out bool           // Expected output
+		err string         // Expected error
+	}
+
+	tests := []testData{
+		{
+			xml: xmldoc.WithText("test", "true"),
+			out: true,
+			err: ``,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "false"),
+			out: false,
+			err: ``,
+		},
+
+		{
+			xml: xmldoc.WithText("test", "bad"),
+			err: `/test: invalid bool: "bad"`,
+		},
+	}
+
+	for _, test := range tests {
+		out, err := decodeBool(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:          %s\n"+
+				"error expected: %q\n"+
+				"error present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+
+		if err.Error() == "" && out != test.out {
+			t.Errorf("output mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %v\n"+
+				"present:  %v\n",
+				test.xml.EncodeString(nil), test.out, out)
+		}
+	}
+}
