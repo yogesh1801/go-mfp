6e842e8a1cc4e1b283550cb4d84f99cd4cc66c05
Alexander Pevzner <pzz@apevzner.com>
2025-03-02 02:24:57 +0300

    escl: added DecodeScannerStatus and ScannerStatus.ToXML

--- escl/scannerstatus.go
+++ escl/scannerstatus.go
@@ -8,7 +8,10 @@
 
 package escl
 
-import "github.com/alexpevzner/mfp/optional"
+import (
+	"github.com/alexpevzner/mfp/optional"
+	"github.com/alexpevzner/mfp/xmldoc"
+)
 
 // ScannerStatus represents the scanner status.
 //
@@ -18,4 +21,85 @@ type ScannerStatus struct {
 	State    ScannerState           // Overall scanner state
 	ADFState optional.Val[ADFState] // ADF state
 	Jobs     []JobInfo              // State of particular jobs
+}
+
+// DecodeScannerStatus decodes [ScannerStatus] from the XML tree.
+func DecodeScannerStatus(root xmldoc.Element) (
+	status ScannerStatus, err error) {
+
+	defer func() { err = xmldoc.XMLErrWrap(root, err) }()
+
+	// Lookup relevant XML elements
+	ver := xmldoc.Lookup{Name: NsPWG + ":Version", Required: true}
+	state := xmldoc.Lookup{Name: NsPWG + ":State", Required: true}
+	adfState := xmldoc.Lookup{Name: NsScan + ":AdfState"}
+	jobs := xmldoc.Lookup{Name: NsScan + ":Jobs"}
+
+	missed := root.Lookup(&ver, &state, &adfState, &jobs)
+	if missed != nil {
+		err = xmldoc.XMLErrMissed(missed.Name)
+		return
+	}
+
+	// Decode elements
+	status.Version, err = decodeVersion(ver.Elem)
+	if err != nil {
+		return
+	}
+
+	status.State, err = decodeScannerState(state.Elem)
+	if err != nil {
+		return
+	}
+
+	if adfState.Found {
+		var s ADFState
+		s, err = decodeADFState(state.Elem)
+		if err != nil {
+			return
+		}
+		status.ADFState = optional.New(s)
+	}
+
+	if jobs.Found {
+		for _, elem := range jobs.Elem.Children {
+			if elem.Name == NsScan+":JobInfo" {
+				var info JobInfo
+				info, err = decodeJobInfo(elem)
+				if err != nil {
+					err = xmldoc.XMLErrWrap(jobs.Elem, err)
+					return
+				}
+
+				status.Jobs = append(status.Jobs, info)
+			}
+		}
+	}
+
+	return
+}
+
+// ToXML generates XML tree for the [ScannerStatus].
+func (status ScannerStatus) ToXML() xmldoc.Element {
+	elm := xmldoc.Element{
+		Name: NsScan + ":ScannerStatus",
+		Children: []xmldoc.Element{
+			status.Version.toXML(NsPWG + ":Version"),
+			status.State.toXML(NsPWG + ":State"),
+		},
+	}
+
+	if status.ADFState != nil {
+		elm.Children = append(elm.Children,
+			(*status.ADFState).toXML(NsScan+":AdfState"))
+	}
+
+	if status.Jobs != nil {
+		for _, job := range status.Jobs {
+			chld := job.toXML(NsScan + ":JobInfo")
+			elm.Children = append(elm.Children, chld)
+		}
+	}
+
+	return elm
 }
