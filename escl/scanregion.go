a4bfa6d12d6b74e67704d2e78d8d9e97ef4923ec
Alexander Pevzner <pzz@apevzner.com>
2025-03-05 11:40:46 +0300

    escl: ScanRegion encoding/decoding and tests

--- escl/scanregion.go
+++ escl/scanregion.go
@@ -8,14 +8,82 @@
 
 package escl
 
-import "github.com/alexpevzner/mfp/util/optional"
+import (
+	"strconv"
+
+	"github.com/alexpevzner/mfp/util/xmldoc"
+)
 
 // ScanRegion defines the desired scan region
 type ScanRegion struct {
-	XOffset            int                // Horizontal offset, 0-based
-	YOffset            int                // Vertical offset, 0-based
-	Width              int                // Region width
-	Height             int                // Region height
-	ContentRegionUnits Units              // Always ThreeHundredthsOfInches
-	MustHonor          optional.Val[bool] // Must override edge detection
+	XOffset            int   // Horizontal offset, 0-based
+	YOffset            int   // Vertical offset, 0-based
+	Width              int   // Region width
+	Height             int   // Region height
+	ContentRegionUnits Units // Always ThreeHundredthsOfInches
+}
+
+// decodeScanRegion decodes [ScanRegion] from the XML tree
+func decodeScanRegion(root xmldoc.Element) (reg ScanRegion, err error) {
+	defer func() { err = xmldoc.XMLErrWrap(root, err) }()
+
+	// Lookup relevant XML elements
+	xoff := xmldoc.Lookup{Name: NsPWG + ":XOffset", Required: true}
+	yoff := xmldoc.Lookup{Name: NsPWG + ":YOffset", Required: true}
+	wid := xmldoc.Lookup{Name: NsPWG + ":Width", Required: true}
+	hei := xmldoc.Lookup{Name: NsPWG + ":Height", Required: true}
+	units := xmldoc.Lookup{Name: NsPWG + ":ContentRegionUnits",
+		Required: true}
+
+	missed := root.Lookup(&xoff, &yoff, &wid, &hei, &units)
+	if missed != nil {
+		err = xmldoc.XMLErrMissed(missed.Name)
+		return
+	}
+
+	// Decode elements
+	reg.XOffset, err = decodeNonNegativeInt(xoff.Elem)
+	if err == nil {
+		reg.YOffset, err = decodeNonNegativeInt(yoff.Elem)
+	}
+	if err == nil {
+		reg.Width, err = decodeNonNegativeInt(wid.Elem)
+	}
+	if err == nil {
+		reg.Height, err = decodeNonNegativeInt(hei.Elem)
+	}
+	if err == nil {
+		reg.ContentRegionUnits, err = decodeUnits(units.Elem)
+	}
+
+	return
+}
+
+// toXML generates XML tree for the [ScanRegion].
+func (r ScanRegion) toXML(name string) xmldoc.Element {
+	elm := xmldoc.Element{
+		Name: name,
+		Children: []xmldoc.Element{
+			{
+				Name: NsPWG + ":XOffset",
+				Text: strconv.FormatUint(uint64(r.XOffset), 10),
+			},
+			{
+				Name: NsPWG + ":YOffset",
+				Text: strconv.FormatUint(uint64(r.YOffset), 10),
+			},
+			{
+				Name: NsPWG + ":Width",
+				Text: strconv.FormatUint(uint64(r.Width), 10),
+			},
+			{
+				Name: NsPWG + ":Height",
+				Text: strconv.FormatUint(uint64(r.Height), 10),
+			},
+			ThreeHundredthsOfInches.toXML(
+				NsPWG + ":ContentRegionUnits"),
+		},
+	}
+
+	return elm
 }
---
+++ escl/scanregion_test.go
@@ -0,0 +1,227 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Scan region test.
+
+package escl
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/alexpevzner/mfp/util/xmldoc"
+)
+
+// TestScanRegion tests ScanRegion
+func TestScanRegion(t *testing.T) {
+	type testData struct {
+		reg ScanRegion
+		xml xmldoc.Element
+	}
+
+	tests := []testData{
+		{
+			// Full-data test
+			reg: ScanRegion{
+				XOffset:            5,
+				YOffset:            10,
+				Width:              2551,
+				Height:             3508,
+				ContentRegionUnits: ThreeHundredthsOfInches,
+			},
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.reg.toXML(NsPWG + ":ScanRegion")
+		if !reflect.DeepEqual(xml, test.xml) {
+			t.Errorf("ToXML:\nexpected: %s\npresent: %s\n",
+				test.xml.EncodeString(NsMap),
+				xml.EncodeString(NsMap))
+		}
+
+		reg, err := decodeScanRegion(xml)
+		if err != nil {
+			t.Errorf("decodeScanRegion: %s", err)
+			continue
+		}
+
+		if !reflect.DeepEqual(reg, test.reg) {
+			t.Errorf("decodeScanRegion:\n"+
+				"expected: %#v\npresent:  %#v\n",
+				test.reg, reg)
+		}
+	}
+}
+
+// TestScanRegionDecodeErrors tests ScanRegion decode errors
+func TestScanRegionDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element
+		err string
+	}
+
+	tests := []testData{
+		// Tests for missed required elements
+		{
+			// Missed XOffset
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:XOffset: missed`,
+		},
+
+		{
+			// Missed YOffset
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:YOffset: missed`,
+		},
+
+		{
+			// Missed Width
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:Width: missed`,
+		},
+
+		{
+			// Missed Height
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:Height: missed`,
+		},
+
+		{
+			// Missed ContentRegionUnits
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+			),
+			err: `/pwg:ScanRegion/pwg:ContentRegionUnits: missed`,
+		},
+
+		// Tests for invalid elements data
+		{
+			// Invalid XOffset
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "bad"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:XOffset: invalid int: "bad"`,
+		},
+
+		{
+			// Invalid YOffset
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "bad"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:YOffset: invalid int: "bad"`,
+		},
+
+		{
+			// Invalid Width
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "bad"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:Width: invalid int: "bad"`,
+		},
+
+		{
+			// Invalid Height
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "bad"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"escl:ThreeHundredthsOfInches"),
+			),
+			err: `/pwg:ScanRegion/pwg:Height: invalid int: "bad"`,
+		},
+
+		{
+			// Invalid XOffset
+			xml: xmldoc.WithChildren(
+				NsPWG+":ScanRegion",
+				xmldoc.WithText(NsPWG+":XOffset", "5"),
+				xmldoc.WithText(NsPWG+":YOffset", "10"),
+				xmldoc.WithText(NsPWG+":Width", "2551"),
+				xmldoc.WithText(NsPWG+":Height", "3508"),
+				xmldoc.WithText(NsPWG+":ContentRegionUnits",
+					"bad"),
+			),
+			err: `/pwg:ScanRegion/pwg:ContentRegionUnits: invalid Units: "bad"`,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := decodeScanRegion(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("%s\nexpected: %q\npresent:  %q",
+				test.xml.EncodeString(NsMap),
+				test.err, err)
+		}
+	}
+}
