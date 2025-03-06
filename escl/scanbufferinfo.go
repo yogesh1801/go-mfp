da09f095d52f7c0ca2794528bdfe1adf989396c6
Alexander Pevzner <pzz@apevzner.com>
2025-03-06 12:39:27 +0300

    escl: added ScanBufferInfo

---
+++ escl/scanbufferinfo.go
@@ -0,0 +1,81 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Scan Buffer Info
+
+package escl
+
+import (
+	"strconv"
+
+	"github.com/alexpevzner/mfp/util/xmldoc"
+)
+
+// ScanBufferInfo is the scanner response, that represents the negotiated
+// (adjusted by the scanner) scanning parameters ([ScanSettings]) and image
+// parameters estimation.
+//
+// eSCL Technical Specification, 10.
+type ScanBufferInfo struct {
+	ScanSettings ScanSettings // Returned scanning parameters
+	ImageWidth   int          // Estimated image width
+	ImageHeight  int          // Estimated image height
+	BytesPerLine int          // Bytes per Line size of uncompressed image
+}
+
+// DecodeScanBufferInfo decodes [ScanBufferInfo] from the XML tree.
+func DecodeScanBufferInfo(root xmldoc.Element) (
+	info ScanBufferInfo, err error) {
+
+	defer func() { err = xmldoc.XMLErrWrap(root, err) }()
+
+	// Lookup relevant XML elements
+	ss := xmldoc.Lookup{Name: NsScan + ":ScanSettings", Required: true}
+	wid := xmldoc.Lookup{Name: NsScan + ":ImageWidth", Required: true}
+	hei := xmldoc.Lookup{Name: NsScan + ":ImageHeight", Required: true}
+	bpl := xmldoc.Lookup{Name: NsScan + ":BytesPerLine", Required: true}
+
+	missed := root.Lookup(&ss, &wid, &hei, &bpl)
+	if missed != nil {
+		err = xmldoc.XMLErrMissed(missed.Name)
+		return
+	}
+
+	// Decode elements
+	info.ScanSettings, err = DecodeScanSettings(ss.Elem)
+	if err == nil {
+		info.ImageWidth, err = decodeNonNegativeInt(wid.Elem)
+	}
+	if err == nil {
+		info.ImageHeight, err = decodeNonNegativeInt(hei.Elem)
+	}
+	if err == nil {
+		info.BytesPerLine, err = decodeNonNegativeInt(bpl.Elem)
+	}
+
+	return
+}
+
+// ToXML generates XML tree for the [ScanBufferInfo].
+func (info ScanBufferInfo) ToXML() xmldoc.Element {
+	elm := xmldoc.Element{
+		Name: NsScan + ":ScanBufferInfo",
+		Children: []xmldoc.Element{
+			info.ScanSettings.ToXML(),
+			xmldoc.WithText(NsScan+":ImageWidth",
+				strconv.FormatUint(
+					uint64(info.ImageWidth), 10)),
+			xmldoc.WithText(NsScan+":ImageHeight",
+				strconv.FormatUint(
+					uint64(info.ImageHeight), 10)),
+			xmldoc.WithText(NsScan+":BytesPerLine",
+				strconv.FormatUint(
+					uint64(info.BytesPerLine), 10)),
+		},
+	}
+
+	return elm
+}
---
+++ escl/scanbufferinfo_test.go
@@ -0,0 +1,217 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Scan Buffer Info test
+
+package escl
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/alexpevzner/mfp/util/xmldoc"
+)
+
+// testScanBufferInfo contains example of the initialized
+// ScanBufferInfo structure
+var testScanBufferInfo = ScanBufferInfo{
+	ScanSettings: testScanSettings,
+	ImageWidth:   2551,
+	ImageHeight:  3508,
+	BytesPerLine: 7653, // 2551 * 3
+}
+
+// TestScanBufferInfo tests [ScanBufferInfo] conversion
+// to and from the XML
+func TestScanBufferInfo(t *testing.T) {
+	type testData struct {
+		info ScanBufferInfo
+		xml  xmldoc.Element
+	}
+
+	tests := []testData{
+		{
+			// Full data test
+			info: testScanBufferInfo,
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		xml := test.info.ToXML()
+		if !xml.Similar(test.xml) {
+			t.Errorf("encode mismatch:\n"+
+				"expected: %s\n"+
+				"present:  %s\n",
+				test.xml.EncodeString(nil),
+				xml.EncodeString(nil))
+		}
+
+		info, err := DecodeScanBufferInfo(test.xml)
+		if err != nil {
+			t.Errorf("decode error:\n"+
+				"input: %s\n"+
+				"error:  %s\n",
+				test.xml.EncodeString(nil), err)
+			continue
+		}
+
+		if !reflect.DeepEqual(info, test.info) {
+			t.Errorf("decode mismatch:\n"+
+				"expected: %#v\n"+
+				"present:  %#v\n",
+				test.info, info)
+		}
+	}
+}
+
+// TestScanBufferInfoDecodeErrors tests [ScanBufferInfo] XML decode
+// errors handling
+func TestScanBufferInfoDecodeErrors(t *testing.T) {
+	type testData struct {
+		xml xmldoc.Element
+		err string
+	}
+
+	tests := []testData{
+		// Tests for missed elements
+		{
+			// Missed ScanSettings
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+			err: `/scan:ScanBufferInfo/scan:ScanSettings: missed`,
+		},
+
+		{
+			// Missed ImageWidth
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+			err: `/scan:ScanBufferInfo/scan:ImageWidth: missed`,
+		},
+
+		{
+			// Missed ImageHeight
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+			err: `/scan:ScanBufferInfo/scan:ImageHeight: missed`,
+		},
+
+		{
+			// Missed BytesPerLine
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+			),
+			err: `/scan:ScanBufferInfo/scan:BytesPerLine: missed`,
+		},
+
+		// Tests for invalid elements
+		{
+			// Invalid ScanSettings
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				xmldoc.WithText(NsScan+":ScanSettings", "bad"),
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+			err: `/scan:ScanBufferInfo/scan:ScanSettings/pwg:Version: missed`,
+		},
+
+		{
+			// Invalid ImageWidth
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageWidth", "bad"),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+			err: `/scan:ScanBufferInfo/scan:ImageWidth: invalid int: "bad"`,
+		},
+
+		{
+			// Invalid ImageHeight
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":ImageHeight", "bad"),
+				xmldoc.WithText(NsScan+":BytesPerLine",
+					"7653"),
+			),
+			err: `/scan:ScanBufferInfo/scan:ImageHeight: invalid int: "bad"`,
+		},
+
+		{
+			// Invalid BytesPerLine
+			xml: xmldoc.WithChildren(
+				NsScan+":ScanBufferInfo",
+				testScanSettings.ToXML(),
+				xmldoc.WithText(NsScan+":ImageWidth",
+					"2551"),
+				xmldoc.WithText(NsScan+":ImageHeight",
+					"3508"),
+				xmldoc.WithText(NsScan+":BytesPerLine", "bad"),
+			),
+			err: `/scan:ScanBufferInfo/scan:BytesPerLine: invalid int: "bad"`,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := DecodeScanBufferInfo(test.xml)
+		if err == nil {
+			err = errors.New("")
+		}
+
+		if err.Error() != test.err {
+			t.Errorf("error mismatch:\n"+
+				"input:    %s\n"+
+				"expected: %q\n"+
+				"present:  %q\n",
+				test.xml.EncodeString(nil), test.err, err)
+		}
+	}
+}
