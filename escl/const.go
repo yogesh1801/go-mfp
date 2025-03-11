79ac1ed59a280e30b616dfdc372da2b656b7e606
Alexander Pevzner <pzz@apevzner.com>
2025-03-11 17:22:39 +0300

    escl: added HTTPContentType constant

---
+++ escl/const.go
@@ -0,0 +1,12 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// eSCL core protocol
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// eSCL constants
+
+package escl
+
+// HTTPContentType is the HTTP Content-Type value for eSCL messages.
+const HTTPContentType = "text/xml"
