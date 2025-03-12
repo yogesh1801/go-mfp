dc9778699e406ff910a90cf2c5609b424b00fa01
Alexander Pevzner <pzz@apevzner.com>
2025-03-12 17:03:21 +0300

    abstract: cosmetic

--- abstract/dimension.go
+++ abstract/dimension.go
@@ -4,7 +4,7 @@
 // Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
 // See LICENSE for license terms and conditions
 //
-// Coordinates
+// Dimension (Coordinates)
 
 package abstract
 
