92576a65aaf8894e252c57e1f2976079a155aca3
Alexander Pevzner <pzz@apevzner.com>
2025-05-09 18:24:52 +0300

    testutils: added a full-size A4/Letter test image sample at 600 DPI

--- internal/testutils/images.go
+++ internal/testutils/images.go
@@ -15,11 +15,18 @@ import (
 
 // Images contains samples of test images of various formats and sizes
 var Images struct {
+	// Small images in various formats
 	BMP100x75  []byte
 	JPEG100x75 []byte
 	PDF100x75  []byte
 	PNG100x75  []byte
 	TIFF100x75 []byte
+
+	// This page is suitable as both A4 and Letter image sample
+	// at 600 DPI:
+	//   Letter: 5100x6600
+	//   A4:     4960x7016
+	PNG5100x7016 []byte
 }
 
 //go:embed "data/UEIT-100x75.bmp"
@@ -37,10 +44,14 @@ var imagePNG100x75 []byte
 //go:embed "data/UEIT-100x75.tiff"
 var imageTIFF100x75 []byte
 
+//go:embed "data/testpage-5100x7016.png"
+var imagePNG5100x7016 []byte
+
 func init() {
 	Images.BMP100x75 = imageBMP100x75
 	Images.JPEG100x75 = imageJPEG100x75
 	Images.PDF100x75 = imagePDF100x75
 	Images.PNG100x75 = imagePNG100x75
 	Images.TIFF100x75 = imageTIFF100x75
+	Images.PNG5100x7016 = imagePNG5100x7016
 }
