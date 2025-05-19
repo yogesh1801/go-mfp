666dc6a92d3097668e8194f0771c5d50195c1296
Alexander Pevzner <pzz@apevzner.com>
2025-05-19 16:42:34 +0300

    imgconv: added Row.Slice and Row.Fill methods

--- imgconv/row.go
+++ imgconv/row.go
@@ -17,6 +17,12 @@ type Row interface {
 
 	// At returns pixel at the specified position.
 	At(x int) color.Color
+
+	// Slice returns a [low:high] sub-slice of the original Row.
+	Slice(low, high int) Row
+
+	// Fill fills Row with the pixels of the specified color
+	Fill(c color.Color)
 }
 
 // NewRow returns the new [Row] of the specified width and [color.Model].
@@ -55,6 +61,15 @@ func (r RowEmpty) At(x int) color.Color {
 	return color.White
 }
 
+// Slice returns a [low:high] sub-slice of the original Row.
+func (r RowEmpty) Slice(low, high int) Row {
+	return RowEmpty{}
+}
+
+// Fill fills Row with the pixels of the specified color
+func (r RowEmpty) Fill(c color.Color) {
+}
+
 // RowGray8 represents a row of 8-bit grayscale image.
 type RowGray8 []color.Gray
 
@@ -66,6 +81,23 @@ func (r RowGray8) Width() int {
 // At returns pixel at the specified position as [color.Color].
 func (r RowGray8) At(x int) color.Color {
 	return r.GrayAt(x)
+}
+
+// Slice returns a [low:high] sub-slice of the original Row.
+func (r RowGray8) Slice(low, high int) Row {
+	return r[low:high]
+}
+
+// Fill fills Row with the pixels of the specified color
+func (r RowGray8) Fill(c color.Color) {
+	c2, ok := c.(color.Gray)
+	if !ok {
+		c2 = color.GrayModel.Convert(c).(color.Gray)
+	}
+
+	for i := range r {
+		r[i] = c2
+	}
 }
 
 // GrayAt returns pixel at the specified position as [color.Gray].
@@ -86,6 +118,23 @@ func (r RowGray16) At(x int) color.Color {
 	return r.Gray16At(x)
 }
 
+// Slice returns a [low:high] sub-slice of the original Row.
+func (r RowGray16) Slice(low, high int) Row {
+	return r[low:high]
+}
+
+// Fill fills Row with the pixels of the specified color
+func (r RowGray16) Fill(c color.Color) {
+	c2, ok := c.(color.Gray16)
+	if !ok {
+		c2 = color.Gray16Model.Convert(c).(color.Gray16)
+	}
+
+	for i := range r {
+		r[i] = c2
+	}
+}
+
 // Gray16At returns pixel at the specified position as [color.Gray16].
 func (r RowGray16) Gray16At(x int) color.Gray16 {
 	return r[x]
@@ -104,6 +153,23 @@ func (r RowRGBA32) At(x int) color.Color {
 	return r.RGBAAt(x)
 }
 
+// Slice returns a [low:high] sub-slice of the original Row.
+func (r RowRGBA32) Slice(low, high int) Row {
+	return r[low:high]
+}
+
+// Fill fills Row with the pixels of the specified color
+func (r RowRGBA32) Fill(c color.Color) {
+	c2, ok := c.(color.RGBA)
+	if !ok {
+		c2 = color.RGBAModel.Convert(c).(color.RGBA)
+	}
+
+	for i := range r {
+		r[i] = c2
+	}
+}
+
 // RGBAAt returns pixel at the specified position as [color.RGBA].
 func (r RowRGBA32) RGBAAt(x int) color.RGBA {
 	return r[x]
@@ -120,6 +186,23 @@ func (r RowRGBA64) Width() int {
 // At returns pixel at the specified position as [color.Color].
 func (r RowRGBA64) At(x int) color.Color {
 	return r.RGBA64At(x)
+}
+
+// Slice returns a [low:high] sub-slice of the original Row.
+func (r RowRGBA64) Slice(low, high int) Row {
+	return r[low:high]
+}
+
+// Fill fills Row with the pixels of the specified color
+func (r RowRGBA64) Fill(c color.Color) {
+	c2, ok := c.(color.RGBA64)
+	if !ok {
+		c2 = color.RGBA64Model.Convert(c).(color.RGBA64)
+	}
+
+	for i := range r {
+		r[i] = c2
+	}
 }
 
 // RGBA64At returns pixel at the specified position as [color.RGBA64].
