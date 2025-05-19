8af51c781fb921ff4be29a6c55ad184e99517da0
Alexander Pevzner <pzz@apevzner.com>
2025-05-19 10:31:04 +0300

    wsscan: added package template

---
+++ proto/wsscan/README.md
@@ -0,0 +1,10 @@
+# WSD core protocol
+
+```
+import "github.com/OpenPrinting/go-mfp/proto/wsscan"
+```
+
+This package provides WS-Scan core protocol implementation.
+
+<!-- vim:ts=8:sw=4:et:textwidth=72
+-->
---
+++ proto/wsscan/doc.go
@@ -0,0 +1,9 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// WS-Scan core protocol
+//
+// Copyright (C) 2024 and up by go-mfp authors.
+// See LICENSE for license terms and conditions
+//
+// Package documentation
+
+package wsscan
