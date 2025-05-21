97a75f6c62d2fbfaafd0c5806c841edb34147e69
Alexander Pevzner <pzz@apevzner.com>
2025-05-21 09:36:35 +0300

    imgconv: added one more resizer test

--- imgconv/resizer_test.go
+++ imgconv/resizer_test.go
@@ -183,6 +183,17 @@ func TestResizer(t *testing.T) {
 			data: testutils.Images.PNG100x75rgb8,
 			rect: image.Rect(25, 25, 125, 100),
 		},
+
+		{
+			//  ################
+			//  ###**********###   * - source
+			//  ###**********###   # - target
+			//  ###**********###
+			//  ###**********###
+			//  ################
+			data: testutils.Images.PNG100x75rgb8,
+			rect: image.Rect(-25, -25, 125, 100),
+		},
 	}
 
 	for _, test := range tests {
