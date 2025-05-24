6e35b1e46a7dafb92960a7ec85a3d56ac90bce6d
Alexander Pevzner <pzz@apevzner.com>
2025-05-24 11:02:48 +0300

    imgconv: 100% test coverage of the resizer achieved!

--- imgconv/resizer_test.go
+++ imgconv/resizer_test.go
@@ -10,12 +10,15 @@ package imgconv
 
 import (
 	"bytes"
+	"errors"
 	"image"
 	"image/color"
 	"image/png"
+	"io"
 	"testing"
 
 	"github.com/OpenPrinting/go-mfp/internal/testutils"
+	"github.com/OpenPrinting/go-mfp/util/generic"
 	"golang.org/x/image/draw"
 )
 
@@ -272,6 +275,15 @@ func TestResizer(t *testing.T) {
 			}
 		}
 
+		// Resizer must return io.EOF after end
+		_, err = resizer.Read(row)
+		if err != io.EOF {
+			t.Errorf("%s: read after end:\n"+
+				"error expected: %s\n"+
+				"error present:  %v\n",
+				test.rect, io.EOF, err)
+		}
+
 		resizer.Close()
 		encoder.Close()
 
@@ -307,5 +319,95 @@ func TestResizer(t *testing.T) {
 		if diff != "" {
 			t.Errorf("%s: image mismatch:\n%s", test.rect, diff)
 		}
+	}
+}
+
+// TestResizerErrors tests how resizer handles I/O errors
+func TestResizerErrors(t *testing.T) {
+	type testData struct {
+		model    color.Model     // Color model
+		wid, hei int             // Input (before resizing) image size
+		lim      int             // Limit of non-error reads
+		rect     image.Rectangle // Target size
+	}
+
+	tests := []testData{
+		{
+			//     **********      *  - source
+			//     **######**      #  - target
+			//  >> **######**      >> - lim
+			//     **********
+			model: color.RGBAModel,
+			wid:   100,
+			hei:   75,
+			lim:   37,
+			rect:  image.Rect(25, 25, 75, 50),
+		},
+
+		{
+			//     ################
+			//     ###**********###   *  - source
+			//     ###**********###   #  - target
+			//  >> ###**********###   >> - lim
+			//     ###**********###
+			//     ################
+			model: color.RGBAModel,
+			wid:   100,
+			hei:   75,
+			lim:   50,
+			rect:  image.Rect(-25, -25, 125, 100),
+		},
+
+		{
+			//  >> **********      *  - source
+			//     **######**      #  - target
+			//     **######**      >> - lim
+			//     **********
+			model: color.RGBAModel,
+			wid:   100,
+			hei:   75,
+			lim:   10,
+			rect:  image.Rect(25, 25, 75, 50),
+		},
+	}
+
+	for _, test := range tests {
+		// Create image conversion pipeline
+		ioerr := errors.New("I/O error")
+
+		decoder := newDecoderWithError(test.model,
+			test.wid, test.hei, test.lim, ioerr)
+		resizer := NewResizer(decoder, test.rect)
+
+		// Rows until limResized expected to be OK
+		limResized := test.lim - test.rect.Min.Y
+		limResized = generic.Max(0, limResized)
+		limResized = generic.Min(test.rect.Dy(), limResized)
+
+		row := resizer.NewRow()
+		ok := true
+		for y := 0; ok && y < limResized; y++ {
+			_, err := resizer.Read(row)
+			if err != nil {
+				t.Errorf("%s: unexpected error %s",
+					test.rect, err)
+				ok = false
+			}
+		}
+
+		// All subsequent reads must return ioerr
+		for y := limResized; ok && y < test.rect.Dy(); y++ {
+			_, err := resizer.Read(row)
+			if err != ioerr {
+				t.Errorf("%s: line %d:\n"+
+					"error expected: %s\n"+
+					"error present:  %v\n",
+					test.rect, y, ioerr, err)
+				ok = false
+			}
+		}
+
+		resizer.Close()
+		decoder.Close()
 	}
 }
