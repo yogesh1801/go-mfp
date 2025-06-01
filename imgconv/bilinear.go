a2406f5a8edc574cef908fe351d66535f3463dc2
Alexander Pevzner <pzz@apevzner.com>
2025-06-01 20:14:46 +0300

    imgconv: computing scaling coefficients made many times faster

--- imgconv/bilinear.go
+++ imgconv/bilinear.go
@@ -40,17 +40,74 @@ func makeScaleCoefficients(slen, dlen int) []scaleCoeff {
 	simg := image.NewGray16(srect)
 	dimg := image.NewGray16(drect)
 
-	for s := 0; s < slen; s++ {
+	// Here we use draw.BiLinear.Scale to obtain its
+	// source to destination contributions coefficients.
+	//
+	// We do it, using the following algorithm:
+	//   for S := 0; S < slen; S ++ {
+	//     - create slen * 1 image
+	//     - set point at S to White, all others to Black
+	//     - scale this image into the dlen * 1 size
+	//     - points at the scaled image gives S->D coefficients
+	//   }
+	//
+	// Unfortunately, this is quadratic algorithm and for the
+	// big dimensions it runs very slow.
+	//
+	// To optimize, we assume that source points placed at the
+	// some big enough distance are completely independent, so
+	// we can simultaneously and independently use source
+	// index N, N+distance, N+distance*2 and so on.
+	//
+	// Unfortunately, we don't know the safe distance in advance.
+	//
+	// To estimate, we make the following assumptions:
+	//   - for downscaling, (slen/dlen) * 3 is safe assumption
+	//   - for upscaling, 4 is safe assumption
+	distance := 0
+
+	if slen > dlen {
+		distance = (slen + dlen - 1) / dlen
+		distance *= 3
+	} else {
+		distance = 4
+	}
+
+	groups := (slen + distance - 1) / distance
+	for off := 0; off < distance; off++ {
+		// Fill image with Black color, once for all groups
 		draw.Draw(simg, srect, image.Black, image.ZP, draw.Over)
-		simg.Set(s, 0, color.White)
+
+		// Set one pixel at the each group to While
+		for group := 0; group < groups; group++ {
+			s := group*distance + off
+			if s >= slen {
+				continue
+			}
+
+			simg.Set(s, 0, color.White)
+		}
+
+		// Scale slen*1 -> dlen*1
 		draw.BiLinear.Scale(dimg, drect, simg, srect, draw.Over, nil)
 
+		// Scan the scaled image. D-coordinate matches
+		// position at that image, S-coordinates we must
+		// recover by counting Black gaps between colored
+		// groups of points.
+		s := off
+		prev := uint16(0)
 		for d := 0; d < dlen; d++ {
-			w := float32(dimg.Gray16At(d, 0).Y) / 0xffff
-			if w != 0 {
+			c := dimg.Gray16At(d, 0).Y
+			if c != 0 {
+				w := float32(c) / 0xffff
 				cff := scaleCoeff{S: s, D: d, W: w}
 				coeffs = append(coeffs, cff)
+			} else if prev != 0 {
+				s += distance
 			}
+
+			prev = c
 		}
 	}
 
