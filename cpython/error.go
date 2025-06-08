666abbc0a4afdeb2f89ddd2e9daa51e0f740350a
Alexander Pevzner <pzz@apevzner.com>
2025-06-08 19:49:29 +0300

    cpython: cosmetic

--- cpython/error.go
+++ cpython/error.go
@@ -1,5 +1,5 @@
 // MFP - Miulti-Function Printers and scanners toolkit
-// Abstract definition for printer and scanner interfaces
+// CPython binding.
 //
 // Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
 // See LICENSE for license terms and conditions
