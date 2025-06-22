c8bd3dc68fb57b2b72393ceb0d2240c7824facb1
Alexander Pevzner <pzz@apevzner.com>
2025-06-22 14:03:23 +0300

    scriptable: added package skeleton

---
+++ scriptable/README.md
@@ -0,0 +1,10 @@
+# CPython binding
+
+```
+import "github.com/OpenPrinting/go-mfp/scriptable"
+```
+
+This package adds scripting to printer and scanner servers.
+
+<!-- vim:ts=8:sw=4:et:textwidth=72
+-->
---
+++ scriptable/doc.go
@@ -0,0 +1,9 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// Print and scam servers with added scriptability.
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Package documentation
+
+package scriptable
