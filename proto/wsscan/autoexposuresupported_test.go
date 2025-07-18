dfc6b8bc44564d6c4342fb7365074aca3f662c21
Yogesh <yogeshsingla481@gmail.com>
2025-07-18 20:55:58 +0530

    wsscan: added booleanelement

--- proto/wsscan/autoexposuresupported_test.go
+++ proto/wsscan/autoexposuresupported_test.go
@@ -1,7 +1,7 @@
 // MFP - Multi-Function Printers and scanners toolkit
 // WS-Scan core protocol
 //
-// Copyright (C) 2024 and up by Yogesh Singla
+// Copyright (C) 2024 and up by Yogesh Singla (yogeshsingla481@gmail.com)
 // See LICENSE for license terms and conditions
 //
 // Test for auto exposure supported
---
+++ proto/wsscan/booleanelement.go
@@ -0,0 +1,43 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// WS-Scan core protocol
+//
+// Copyright (C) 2024 and up by Yogesh Singla (yogeshsingla481@gmail.com)
+// See LICENSE for license terms and conditions
+//
+// BooleanElement: reusable boolean XML element type
+
+package wsscan
+
+import (
+	"errors"
+	"strings"
+)
+
+// BooleanElement is a string type representing a boolean XML value.
+// Allowed values: "0", "1", "false", "true" (case-insensitive, whitespace ignored).
+type BooleanElement string
+
+// IsValid returns true if the value is a valid BooleanElement value.
+func (b BooleanElement) IsValid() bool {
+	switch strings.ToLower(strings.TrimSpace(string(b))) {
+	case "0", "1", "false", "true":
+		return true
+	default:
+		return false
+	}
+}
+
+// Bool returns the boolean value of BooleanElement.
+// Returns true for "1" or "true", false for "0" or "false".
+func (b BooleanElement) Bool() (bool, error) {
+	switch strings.ToLower(strings.TrimSpace(string(b))) {
+	case "1", "true":
+		return true, nil
+	case "0", "false":
+		return false, nil
+	default:
+		return false, errors.New(
+			"invalid value for BooleanElement: must be 0, 1, false, or true",
+		)
+	}
+}
