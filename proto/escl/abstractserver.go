ff631bcfbcfe0b1c6e09c7ae1b7065b8ca032b2d
Alexander Pevzner <pzz@apevzner.com>
2025-07-23 19:02:52 +0300

    escl: use transport.ServerQuery instead of the abstractServerQuery

--- proto/escl/abstractserver.go
+++ proto/escl/abstractserver.go
@@ -11,16 +11,13 @@ package escl
 import (
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 	"path"
 	"strings"
 	"sync"
-	"sync/atomic"
 
 	"github.com/OpenPrinting/go-mfp/abstract"
-	"github.com/OpenPrinting/go-mfp/log"
 	"github.com/OpenPrinting/go-mfp/transport"
 	"github.com/OpenPrinting/go-mfp/util/missed"
 	"github.com/OpenPrinting/go-mfp/util/optional"
@@ -57,119 +54,6 @@ type AbstractServerOptions struct {
 	BasePath string
 }
 
-// abstractServerQuery maintains an AbstractServer query processing
-// context, allowing per-request centralized logging and hooking.
-//
-// It keeps the reference to the original [http.Request] and wraps
-// the corresponding [http.ResponseWriter], passed to the
-// AbstractServer.ServeHTTP
-type abstractServerQuery struct {
-	log                 *log.Record  // Log record for the query
-	*http.Request                    // Incoming request
-	http.ResponseWriter              // Underlying http.ResponseWriter
-	status              atomic.Int32 // HTTP status, 0 if not known yet
-}
-
-// newAbstractServerQuery returns the new abstractServerQuery
-func newAbstractServerQuery(srv *AbstractServer,
-	w http.ResponseWriter, rq *http.Request) *abstractServerQuery {
-
-	query := &abstractServerQuery{
-		log:            log.Begin(srv.ctx),
-		Request:        rq,
-		ResponseWriter: w,
-	}
-
-	return query
-}
-
-// RequestHeader returns http.Header of the request
-func (query *abstractServerQuery) RequestHeader() http.Header {
-	return query.Request.Header
-}
-
-// Finish must be called when query processing is finished
-func (query *abstractServerQuery) Finish() {
-	query.log.Commit()
-}
-
-// RequestBody returns body of the http.Request
-func (query *abstractServerQuery) RequestBody() io.ReadCloser {
-	return query.Request.Body
-}
-
-// ResponseHeader returns http.Header of the response
-func (query *abstractServerQuery) ResponseHeader() http.Header {
-	return query.ResponseWriter.Header()
-}
-
-// Write writes response body bytes.
-func (query *abstractServerQuery) Write(data []byte) (int, error) {
-	return query.ResponseWriter.Write(data)
-}
-
-// WriteHeader writes HTTP response header.
-func (query *abstractServerQuery) WriteHeader(status int) {
-	if query.status.CompareAndSwap(0, int32(status)) {
-		query.ResponseWriter.WriteHeader(status)
-		query.log.Debug("HTTP %s %s -- %d %s",
-			query.Method, query.URL,
-			status, http.StatusText(status))
-		query.log.Flush()
-	}
-}
-
-// NoCache set response headers to disable client-side response cacheing.
-func (query *abstractServerQuery) NoCache() {
-	hdr := query.ResponseHeader()
-	hdr.Set("Cache-Control", "no-cache, no-store, must-revalidate")
-	hdr.Set("Pragma", "no-cache")
-	hdr.Set("Expires", "0")
-}
-
-// Reject completes request with a error.
-func (query *abstractServerQuery) Reject(status int, err error) {
-	query.ResponseHeader().Set("Content-Type", "text/plain; charset=utf-8")
-	query.NoCache()
-	query.WriteHeader(status)
-
-	if err == nil {
-		err = errors.New(http.StatusText(status))
-	}
-
-	s := fmt.Sprintf("%3.3d %s\n", status, err)
-	query.Write([]byte(s))
-	query.Write([]byte("\n"))
-}
-
-// Created completes request with the http.StatusCreated
-// status and Location: URL
-func (query *abstractServerQuery) Created(joburi string) {
-	scheme := "http"
-	if query.TLS != nil {
-		scheme = "https"
-	}
-
-	location := scheme + "://" + query.Host + joburi
-
-	query.ResponseHeader().Set("Location", location)
-	query.WriteHeader(http.StatusCreated)
-}
-
-// SendXML sends the XML response.
-func (query *abstractServerQuery) SendXML(xml xmldoc.Element) {
-	query.ResponseHeader().Set("Content-Type", HTTPContentType)
-	query.WriteHeader(http.StatusOK)
-	xml.EncodeIndent(query, NsMap, "  ")
-}
-
-// SendImage sends the scanned image
-func (query *abstractServerQuery) SendImage(file abstract.DocumentFile) {
-	query.ResponseHeader().Set("Content-Type", file.Format())
-	query.WriteHeader(http.StatusOK)
-	io.Copy(query, file)
-}
-
 // NewAbstractServer returns a new [AbstractServer].
 func NewAbstractServer(ctx context.Context,
 	options AbstractServerOptions) *AbstractServer {
@@ -204,37 +88,37 @@ func NewAbstractServer(ctx context.Context,
 // ServeHTTP serves incoming HTTP requests.
 // It implements the [http.Handler] interface.
 func (srv *AbstractServer) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
-	// Create a abstractServerQuery
-	query := newAbstractServerQuery(srv, w, rq)
+	// Create a transport.ServerQuery
+	query := transport.NewServerQuery(srv.ctx, w, rq)
 	defer query.Finish()
 
 	// Dispatch the request
-	if !strings.HasPrefix(query.URL.Path, srv.options.BasePath) {
+	if !strings.HasPrefix(query.RequestURL().Path, srv.options.BasePath) {
 		query.Reject(http.StatusNotFound, nil)
 		return
 	}
 
-	path, _ := missed.StringsCutPrefix(query.URL.Path,
+	path, _ := missed.StringsCutPrefix(query.RequestURL().Path,
 		srv.options.BasePath)
 
 	// Handle {root}-relative requests
-	var action func(*abstractServerQuery)
+	var action func(*transport.ServerQuery)
 
 	srv.lock.Lock()
 
 	switch path {
 	case "ScannerCapabilities":
-		if query.Method == "GET" {
+		if query.RequestMethod() == "GET" {
 			action = srv.getScannerCapabilities
 		}
 
 	case "ScannerStatus":
-		if rq.Method == "GET" {
+		if query.RequestMethod() == "GET" {
 			action = srv.getScannerStatus
 		}
 
 	case "ScanJobs":
-		if rq.Method == "POST" {
+		if query.RequestMethod() == "POST" {
 			action = srv.postScanJobs
 		}
 	}
@@ -243,9 +127,9 @@ func (srv *AbstractServer) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
 	if action == nil && srv.document != nil {
 		joburi := srv.status.Jobs[0].JobURI
 
-		switch rq.Method {
+		switch query.RequestMethod() {
 		case "GET":
-			switch query.URL.Path {
+			switch query.RequestURL().Path {
 			case joburi + "/NextDocument":
 				action = srv.getJobURINextDocument
 			case joburi + "/ScanImageInfo":
@@ -253,7 +137,7 @@ func (srv *AbstractServer) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
 			}
 
 		case "DELETE":
-			if query.URL.Path == joburi {
+			if query.RequestURL().Path == joburi {
 				action = srv.deleteJobURI
 			}
 		}
@@ -269,23 +153,23 @@ func (srv *AbstractServer) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
 }
 
 // getScannerCapabilities handles GET /{root}/ScannerCapabilities request
-func (srv *AbstractServer) getScannerCapabilities(query *abstractServerQuery) {
+func (srv *AbstractServer) getScannerCapabilities(query *transport.ServerQuery) {
 	ver := srv.status.Version
 	xml := fromAbstractScannerCapabilities(ver, srv.caps).ToXML()
-	query.SendXML(xml)
+	query.SendXML(http.StatusOK, NsMap, xml)
 }
 
 // getScannerStatus handles GET /{root}/ScannerStatus request
-func (srv *AbstractServer) getScannerStatus(query *abstractServerQuery) {
+func (srv *AbstractServer) getScannerStatus(query *transport.ServerQuery) {
 	srv.lock.Lock()
 	xml := srv.status.ToXML()
 	srv.lock.Unlock()
 
-	query.SendXML(xml)
+	query.SendXML(http.StatusOK, NsMap, xml)
 }
 
 // postScanJobs handles POST /{root}/ScanJobs
-func (srv *AbstractServer) postScanJobs(query *abstractServerQuery) {
+func (srv *AbstractServer) postScanJobs(query *transport.ServerQuery) {
 	srv.lock.Lock()
 	defer srv.lock.Unlock()
 
@@ -349,7 +233,7 @@ func (srv *AbstractServer) postScanJobs(query *abstractServerQuery) {
 }
 
 // getJobURINextDocument handles GET /{JobUri}/NextDocument
-func (srv *AbstractServer) getJobURINextDocument(query *abstractServerQuery) {
+func (srv *AbstractServer) getJobURINextDocument(query *transport.ServerQuery) {
 	srv.lock.Lock()
 	file, err := srv.document.Next()
 	srv.lock.Unlock()
@@ -364,17 +248,17 @@ func (srv *AbstractServer) getJobURINextDocument(query *abstractServerQuery) {
 		query.Reject(http.StatusServiceUnavailable, err)
 
 	default:
-		query.SendImage(file)
+		query.SendData(http.StatusOK, file.Format(), file)
 	}
 }
 
 // getJobURIScanImageInfo handles GET /{JobUri}/ScanImageInfo
-func (srv *AbstractServer) getJobURIScanImageInfo(query *abstractServerQuery) {
+func (srv *AbstractServer) getJobURIScanImageInfo(query *transport.ServerQuery) {
 	query.Reject(http.StatusNotImplemented, nil)
 }
 
 // deleteJobURI handles DELETE /{JobUri}
-func (srv *AbstractServer) deleteJobURI(query *abstractServerQuery) {
+func (srv *AbstractServer) deleteJobURI(query *transport.ServerQuery) {
 	srv.finish(JobCanceled, JobCanceledByUser)
 	query.WriteHeader(http.StatusOK)
 }
