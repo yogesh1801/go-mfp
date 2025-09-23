c8e38830ab27ebef6bcb0c911fdaf6985b2e092d
Alexander Pevzner <pzz@apevzner.com>
2025-09-23 18:20:37 +0300

    ipp: meaningful content of CUPSGetPPDsRequest moved to PPDFilter structure

--- proto/ipp/cups.go
+++ proto/ipp/cups.go
@@ -89,17 +89,7 @@ type (
 		RequestHeader
 
 		// Operational attributes
-		ExcludeSchemes      []string             `ipp:"exclude-schemes,name"`
-		IncludeSchemes      []string             `ipp:"include-schemes,name"`
-		Limit               optional.Val[int]    `ipp:"limit,(1:MAX)"`
-		PpdMake             optional.Val[string] `ipp:"ppd-make,text"`
-		PpdMakeAndModel     optional.Val[string] `ipp:"ppd-make-and-model,text"`
-		ModelNumber         optional.Val[int]    `ipp:"ppd-model-number"`
-		PpdNaturalLanguage  optional.Val[string] `ipp:"ppd-natural-language,text"`
-		PpdProduct          optional.Val[string] `ipp:"ppd-product,text"`
-		PpdPsversion        optional.Val[string] `ipp:"ppd-psversion,text"`
-		PpdType             optional.Val[string] `ipp:"ppd-type,keyword"`
-		RequestedAttributes []string             `ipp:"requested-attributes,keyword"`
+		PPDFilter
 	}
 
 	// CUPSGetPPDsResponse is the CUPS-Get-PPDs Response.
--- proto/ipp/ppd-attrs.go
+++ proto/ipp/ppd-attrs.go
@@ -8,6 +8,24 @@
 
 package ipp
 
+import "github.com/OpenPrinting/go-mfp/util/optional"
+
+// PPDFilter specifies the subset of PPD files and their attributes,
+// returned by the CUPS-Get-PPDs request.
+type PPDFilter struct {
+	ExcludeSchemes      []string             `ipp:"exclude-schemes,name"`
+	IncludeSchemes      []string             `ipp:"include-schemes,name"`
+	Limit               optional.Val[int]    `ipp:"limit,(1:MAX)"`
+	PpdMake             optional.Val[string] `ipp:"ppd-make,text"`
+	PpdMakeAndModel     optional.Val[string] `ipp:"ppd-make-and-model,text"`
+	ModelNumber         optional.Val[int]    `ipp:"ppd-model-number"`
+	PpdNaturalLanguage  optional.Val[string] `ipp:"ppd-natural-language,text"`
+	PpdProduct          optional.Val[string] `ipp:"ppd-product,text"`
+	PpdPsversion        optional.Val[string] `ipp:"ppd-psversion,text"`
+	PpdType             optional.Val[string] `ipp:"ppd-type,keyword"`
+	RequestedAttributes []string             `ipp:"requested-attributes,keyword"`
+}
+
 // PPDAttributes represents PPD file attributes, as returned by
 // the CUPS-Get-PPDs request
 type PPDAttributes struct {
