b9ce91625d318111a3f7d364c9de7431f82e5fe5
Alexander Pevzner <pzz@apevzner.com>
2025-10-06 20:22:46 +0300

    usb: new proto/usb packages contains USB protocol-level definitions
    The idea is to share USB definitions between USB-related packages
    (usbip, planned host-side USB API etc)

---
+++ proto/usb/doc.go
@@ -0,0 +1,9 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// Virtual USB/IP device emulator for testing and fuzzing
+//
+// Copyright (C) 2025 and up by GO-MFP authors.
+// See LICENSE for license terms and conditions
+//
+// USB protocol definitions
+
+package usb
