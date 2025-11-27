8cc4ef4925af409b37e99935f4784248fe17e86b
Alexander Pevzner <pzz@apevzner.com>
2025-11-27 19:53:07 +0300

    iana: 100% test coverage achieved

---
+++ proto/ipp/iana/lookup_test.go
@@ -0,0 +1,44 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// IANA registrations for IPP
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// LookupAttribute tests
+
+package iana
+
+import (
+	"sort"
+	"testing"
+)
+
+// TestLookupAttributeNotFound tests for attribute names that must not be found
+func TestLookupAttributeNotFound(t *testing.T) {
+	// Prepare test cases
+	paths := []string{
+		"",                                      // Empty string
+		"Not Exist/attribute",                   // Not existing collection
+		"Job Template/not-exist",                // Attribute doesn't exist
+		"Job Template/print-accuracy/not-exist", // Member doesn't exist
+		"Job Template/overrides/overrides",      // Attribute includes self as a member
+	}
+
+	for col := range Collections {
+		paths = append(paths, col)
+	}
+
+	exceptions.ForEach(func(path string) {
+		paths = append(paths, path)
+	})
+
+	sort.Strings(paths)
+
+	// Run tests
+	for _, path := range paths {
+		attr := LookupAttribute(path)
+		if attr != nil {
+			t.Errorf("%q: LookupAttribute must return nil", path)
+		}
+	}
+}
