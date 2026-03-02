7bcc498cb98bb8200a1bd566433eac2eec0a1631
Alexander Pevzner <pzz@apevzner.com>
2026-03-02 17:11:34 +0300

    iana: refactoring, LookupAttribute and LookupSet moved into lookup.go

--- proto/ipp/iana/defs.go
+++ proto/ipp/iana/defs.go
@@ -162,43 +162,6 @@ func (def *DefAttr) String() string {
 	return s
 }
 
-// LookupAttribute returns attribute by its full path.
-// The full path looks as follows:
-//
-//	"Job Template/cover-back/media-col"
-func LookupAttribute(path string) *DefAttr {
-	if exceptions.Contains(path) {
-		return nil
-	}
-
-	splitPath := strings.Split(path, "/")
-	if len(splitPath) < 2 {
-		return nil
-	}
-
-	col := Collections[splitPath[0]]
-	if col == nil {
-		return nil
-	}
-
-	def := col[splitPath[1]]
-	for _, next := range splitPath[2:] {
-		def = def.Member(next)
-	}
-
-	return def
-}
-
-// LookupSet searches attribute by name within set of members.
-func LookupSet(set []map[string]*DefAttr, name string) *DefAttr {
-	for _, m := range set {
-		if def := m[name]; def != nil {
-			return def
-		}
-	}
-	return nil
-}
-
 func init() {
 	for _, b := range borrowings {
 		rcpt := LookupAttribute(b.recipient)
