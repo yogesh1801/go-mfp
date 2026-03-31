34564cb993dcae64dfd7c5769b509d303eac4f49
Alexander Pevzner <pzz@apevzner.com>
2026-03-31 18:02:40 +0300

    testutils: Diff considers two errors equal, if they render to the same text

--- internal/testutils/struct.go
+++ internal/testutils/struct.go
@@ -9,6 +9,7 @@
 package testutils
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -18,7 +19,43 @@ import (
 // Diff returns a pretty-printing report of the difference between
 // two values. If values are equal, it returns empty string.
 func Diff(x, y any) string {
-	return cmp.Diff(x, y)
+	return cmp.Diff(x, y, compareErrorsOption())
+}
+
+// compareErrorsOption returns an cmp.Option that tells
+// Diff to use compareErrors for comparing error values
+func compareErrorsOption() cmp.Option {
+	return cmp.FilterValues(
+		func(x, y interface{}) bool {
+			_, ok1 := x.(error)
+			_, ok2 := y.(error)
+			return ok1 && ok2
+		},
+		cmp.Comparer(compareErrors),
+	)
+}
+
+// compareErrors tells if two errors are equal.
+//
+// Errors that render to the same text message
+// considered equal, even if underlying values
+// are different.
+func compareErrors(e1, e2 interface{}) bool {
+	switch {
+	case e1 == nil && e2 == nil:
+		return true
+	case e1 == nil || e2 == nil:
+		return false
+	}
+
+	err1 := e1.(error)
+	err2 := e2.(error)
+
+	if errors.Is(err1, err2) || errors.Is(err2, err1) {
+		return true
+	}
+
+	return err1.Error() == err2.Error()
 }
 
 // Format formats (pretty-prints) an arbitrary Go value.
