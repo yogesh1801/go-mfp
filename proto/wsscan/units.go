b889702c10c7747af108a45f1a83b3caef714e53
Yogesh Singla <yogeshsingla481@gmail.com>
2026-04-30 15:45:07 +0530

    wsscan: add abstract for scanner element response

---
+++ proto/wsscan/units.go
@@ -0,0 +1,13 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// WS-Scan core protocol
+//
+// Copyright (C) 2024 and up by Yogesh Singla (yogeshsingla481@gmail.com)
+// See LICENSE for license terms and conditions
+//
+// WS-Scan units and conversion constants
+
+package wsscan
+
+// wsscanDPI is the resolution at which WS-Scan dimensions are expressed
+// (thousandths of an inch, i.e. 1/1000").
+const wsscanDPI = 1000
