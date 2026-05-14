e3e1210310e4c5b1ce29ab1d1a20b93ca0de4be0
Alexander Pevzner <pzz@apevzner.com>
2026-05-14 16:06:10 +0300

    wsscan: cosmetic

--- proto/wsscan/abstractserver.go
+++ proto/wsscan/abstractserver.go
@@ -1,3 +1,11 @@
+// MFP - Multi-Function Printers and scanners toolkit
+// WS-Scan core protocol
+//
+// Copyright (C) 2024 and up by Yogesh Singla (yogeshsingla481@gmail.com)
+// See LICENSE for license terms and conditions
+//
+// WS-Scan server on a top of abstract.Scanner
+
 package wsscan
 
 import (
