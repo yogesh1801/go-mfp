6c385669af48a428f629fd13fdb4456e7284d10f
Alexander Pevzner <pzz@apevzner.com>
2026-06-17 19:06:24 +0300

    modeling: added real-world test for decoding/encoding WSD scanner capabilities

--- modeling/model_test.go
+++ modeling/model_test.go
@@ -15,9 +15,13 @@ import (
 	"github.com/OpenPrinting/go-mfp/internal/assert"
 	"github.com/OpenPrinting/go-mfp/internal/testutils"
 	"github.com/OpenPrinting/go-mfp/proto/escl"
+	"github.com/OpenPrinting/go-mfp/proto/wsscan"
 	"github.com/OpenPrinting/go-mfp/util/xmldoc"
 )
 
+// TestKyoceraESCLScannerCapabilities is the real-world test, that
+// verifies that the real Kyocera ECOSYS M2040dn eSCL ScannerCapabilities
+// is properly handled.
 func TestKyoceraESCLScannerCapabilities(t *testing.T) {
 	// Decode Kyocera ScannerCapabilities
 	rd := bytes.NewReader(testutils.Kyocera.
@@ -72,6 +76,73 @@ func TestKyoceraESCLScannerCapabilities(t *testing.T) {
 		t.Errorf("Model.Read: %s", err)
 	}
 
+	scancaps2 = model2.GetESCLScanCaps()
+	diff = testutils.Diff(scancaps, scancaps2)
+	if diff != "" {
+		t.Errorf("Model.Write/Model.Read:\n%s", diff)
+	}
+}
+
+// TestKyoceraWSDScannerCapabilities is the real-world test, that
+// verifies that the real Kyocera ECOSYS M2040dn WSD ScannerCapabilities
+// is properly handled.
+func TestKyoceraWSDScannerCapabilities(t *testing.T) {
+	// Decode Kyocera ScannerCapabilities
+	rd := bytes.NewReader(testutils.Kyocera.
+		ECOSYS.M2040dn.WSD.GetScannerElementsResponse)
+	xml, err := xmldoc.Decode(wsscan.NsMap, rd)
+	assert.NoError(err)
+
+	msg, err := wsscan.DecodeMessage(xml)
+	assert.NoError(err)
+
+	scancaps := msg.Body.(*wsscan.GetScannerElementsResponse)
+
+	// Create a new, empty Model
+	model, err := NewModel()
+	assert.NoError(err)
+
+	defer model.Close()
+
+	// Roll over structExport/structImport
+	obj := structExport(model.py, keywordMapWSD, scancaps)
+	if err := obj.Err(); err != nil {
+		t.Errorf("structExport: %s", err)
+		return
+	}
+
+	var scancaps2 *wsscan.GetScannerElementsResponse
+	err = structImport(obj, keywordMapWSD, &scancaps2)
+	if err != nil {
+		t.Errorf("structImport: %s", err)
+		return
+	}
+
+	diff := testutils.Diff(scancaps, scancaps2)
+	if diff != "" {
+		t.Errorf("structExport/structImport:\n%s", diff)
+	}
+
+	// Roll over Model.Write/Model.Read
+	buf := &bytes.Buffer{}
+
+	model.SetWSDScanCaps(scancaps)
+	err = model.Write(buf)
+	if err != nil {
+		t.Errorf("Model.Write: %s", err)
+	}
+
+	model2, err := NewModel()
+	assert.NoError(err)
+
+	defer model2.Close()
+
+	err = model2.Read("test", buf)
+	if err != nil {
+		t.Errorf("Model.Read: %s", err)
+	}
+
+	scancaps2 = model2.GetWSDScanCaps()
 	diff = testutils.Diff(scancaps, scancaps2)
 	if diff != "" {
 		t.Errorf("Model.Write/Model.Read:\n%s", diff)
